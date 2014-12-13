8dfca2ae406906ba2f64bdf5ccb141426be7b61a
Ray Toal <rtoal@lmu.edu>
2014-12-13 12:16:00 -0800

    First Lua program

---
+++ lua/triple.lua
@@ -0,0 +1,9 @@
+for c = 1, 50 do
+    for b = 1, c do
+        for a = 1, b do
+            if a * a + b * b == c * c then
+                print(string.format("%d, %d, %d", a, b, c))
+            end
+        end
+    end
+end
