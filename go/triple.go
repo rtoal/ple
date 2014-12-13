cab6b44dfa25de604cfd2a793161e4112e36cd2f
Ray Toal <rtoal@lmu.edu>
2014-12-13 12:34:03 -0800

    First Go and first Swift script

---
+++ go/triple.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    for c := 1; c <= 50; c++ {
+        for b := 1; b <= c; b++ {
+            for a := 1; a <= b; a++ {
+                if a * a + b * b == c * c {
+                    fmt.Printf("%d, %d, %d\n", a, b, c)
+                }
+            }
+        }
+    }
+}
