1ca33a871b79f82e199ef87752aca4ce177b015d
Ray Toal <rtoal@lmu.edu>
2014-12-15 10:02:33 -0800

    New Julia script

---
+++ julia/anagrams.jl
@@ -0,0 +1,21 @@
+if length(ARGS) != 1
+  println("Exactly one argument is required")
+  exit(1)
+end
+
+function swap(a, i, j)
+  a[i], a[j] = a[j], a[i]
+end
+
+function generatePermutations(n, a)
+  if n == 1
+    println(join(a, ""))
+  else
+    for i = 1:n
+      generatePermutations(n-1, a)
+      swap(a, (n % 2 == 0 ? i : 1), n)
+    end
+  end
+end
+
+generatePermutations(length(ARGS[1]), split(ARGS[1], ""))
