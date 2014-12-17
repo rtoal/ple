59e0a1ff90b70c18f6873769aabccfd0dc58aeda
Andrew Akers <andrew@akrs.me>
2014-12-17 15:41:41 -0800

    first attempt at anagram TODO: Something is wrong with null terminator

---
+++ c/anagrams.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char* swap (char* source, int i, int j) {
+    char saved = source[i];
+    source[i] = source[j];
+    source[j] = saved;
+}
+
+void generate_permutations (int n, char* a) {
+    if (n == 0) {
+        printf("%s\n", a);
+    } else {
+        for (int i = 0; i <= n; i++) {
+            generate_permutations(n - 1, a);
+            swap(a, n % 2 == 0 ? i : 0, n);
+        }
+    }
+}
+
+
+int main (int argc, const char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Need exactly one argument!\n");
+        return 1;
+    }
+    
+    size_t len = strlen(argv[1]);
+    char * word = malloc(len + 1); // Remember null terminator
+    word = strncpy(word, argv[1], len);
+    
+    generate_permutations(len, word);
+    return 0;
+}
