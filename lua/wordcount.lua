5fe29662c935d11295980433d5e87358b5e7084a
Alex Schneider <aschne19@lion.lmu.edu>
2014-12-19 14:30:46 -0800

    Remove comments and fixed indentation

--- lua/wordcount.lua
+++ lua/wordcount.lua
@@ -5,9 +5,9 @@ for line in io.lines() do
   end)
 end
 
-displayTable = {} -- Because lua doesn't have any concept of ordered tables by keys, we need to order it ourselves
+displayTable = {}
 for word, count in pairs(wordTable) do 
-    table.insert(displayTable, string.format('%s %d', word, count)) 
+  table.insert(displayTable, string.format('%s %d', word, count)) 
 end
 table.sort(displayTable)
 for i,n in ipairs(displayTable) do print(n) end
