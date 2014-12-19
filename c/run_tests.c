40b51275ba9a2b7839401eb2cdd899785f9ab583
Andrew Akers <andrew@akrs.me>
2014-12-18 19:26:15 -0800

    a test

--- c/run_tests.c
+++ c/run_tests.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include "sum_of_even_squares.c"
+#include "wordcount.c"
 
 static int a[] = {7, 3, -8, 4, 1, 0, 11, 2};
 
@@ -17,8 +18,16 @@ void test_sum_of_even_squares() {
     assert(sum_of_even_squares(a, 8) == 84);
 }
 
+void test_find_next_word() {
+    const char* helo = "Hello, world!";
+    char hello[14];
+    strcpy(hello, helo);
+    assert(find_next_word(hello) == &hello[7]);
+}
+
 int main() {
     test_sum_of_even_squares();
+    test_find_next_word();
     printf("All tests passed\n");
     return 0;
 }
