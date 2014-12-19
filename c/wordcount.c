b0f4d86dc6ef9e258c38c8b90953eca3548faada
Andrew Akers <andrew@akrs.me>
2014-12-18 19:57:05 -0800

    find_next_word now returns 0 if it hits a \0

--- c/wordcount.c
+++ c/wordcount.c
@@ -3,14 +3,19 @@
 #include <string.h>
 
 /*
-  Null terminates first word, and returns a pointer to the start of the next word
+  Null terminates first word, and returns a pointer to the start of the next word. If it hits the null
+  terminator, it will return 0.
 */
 char* find_next_word (char *str) {
-    while ((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z') || *str == '\'') {
+    while ((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z') || *str == '\'' || *str == '\0') {
+        if (*str == '\0')
+            return 0;
         str++;
     }
-    *str = '\0'; // Found end of first word
-    while (!((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z') || *str == '\'')) {
+    *str++ = '\0'; // Found end of first word
+    while (!((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z') || *str == '\'') || *str == '\0') {
+        if (*str == '\0')
+            return 0;
         str++;
     }
     return str; // Found begining of second word
