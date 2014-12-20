39a1603a44062e1328fea6c91695c538a68387ea
Andrew Akers <andrew@akrs.me>
2014-12-20 15:19:46 -0800

    remove extranous tests

--- c/run_tests.c
+++ c/run_tests.c
@@ -18,34 +18,8 @@ void test_sum_of_even_squares() {
     assert(sum_of_even_squares(a, 8) == 84);
 }
 
-void test_find_next_word() {
-    const char* helo = "Hello, world!";
-    char hello[14];
-    strcpy(hello, helo);
-    assert(*find_next_word(hello) == 'w');
-
-    const char* dar = "The dårk and størmy night's cat";
-    char *dark = malloc(strlen(dar + 1));
-    strcpy(dark, dar);
-    dark = find_next_word(dark);
-    assert(*dark == 'd');
-    dark = find_next_word(dark);
-    assert(*dark == 'r');
-    dark = find_next_word(dark);
-    assert(*dark == 'a');
-    dark = find_next_word(dark);
-    assert(*dark == 's');
-    dark = find_next_word(dark);
-    assert(*dark == 'r');
-    dark = find_next_word(dark);
-    assert(*dark == 'n');
-    dark = find_next_word(dark);
-    assert(*dark == 'c');
-}
-
 int main() {
     test_sum_of_even_squares();
-    test_find_next_word();
     printf("All tests passed\n");
     return 0;
 }
