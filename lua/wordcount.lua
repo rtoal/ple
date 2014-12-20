ea9080a38421042358789b1b8a7ed631ff016e41
Ray Toal <rtoal@lmu.edu>
2014-12-19 16:23:33 -0800

    Lua word count needs to ignore case and restrict acceptable characters

--- lua/wordcount.lua
+++ lua/wordcount.lua
@@ -1,13 +1,13 @@
 wordTable = {}
 for line in io.lines() do
-  line:gsub('%S+', function(w) 
+  line:lower():gsub('[a-z\']+', function(w)
     wordTable[w] = (wordTable[w] or 0) + 1
   end)
 end
 
 displayTable = {}
-for word, count in pairs(wordTable) do 
-  table.insert(displayTable, string.format('%s %d', word, count)) 
+for word, count in pairs(wordTable) do
+  table.insert(displayTable, string.format('%s %d', word, count))
 end
 table.sort(displayTable)
 for i,n in ipairs(displayTable) do print(n) end
