fa2cfe50e69037ba68a9dda76b7ef523dbcd0181
Ray Toal <rtoal@lmu.edu>
2014-12-20 11:09:08 -0800

    Use inout parameters and remove parens from if and while conditions

--- swift/anagram.swift
+++ swift/anagram.swift
@@ -2,29 +2,28 @@
 
 import Foundation
 
-func generatePermutations (var n : Int, var a : [Character]) {
-    if (n == 0) {
+func swap(inout a: [Character], i : Int, j : Int) {
+    var saved = a[i]
+    a[i] = a[j]
+    a[j] = saved
+}
+
+func generatePermutations(n : Int, inout a : [Character]) {
+    if n == 0 {
         println("".join(a.map{"\($0)"}))
     } else {
         for i in 0...n {
-            generatePermutations(n-1, a)
-            a = swap(a, n % 2 == 0 ? i : 0, n)
+            generatePermutations(n-1, &a)
+            swap(&a, n % 2 == 0 ? i : 0, n)
         }
     }
 }
 
-func swap (var a: [Character], i : Int, j : Int) -> ([Character]) {
-    var saved = a[i]
-    a[i] = a[j]
-    a[j] = saved
-    return a
-}
-
-if (Process.arguments.count != 2) {
+if Process.arguments.count != 2 {
     println("Exactly one argument is required");
     exit(1)
 }
 
 var word = Process.arguments[1]
-generatePermutations((countElements(word) - 1), Array(word))
-
+var charArray = Array(word)
+generatePermutations(countElements(word)-1, &charArray)
