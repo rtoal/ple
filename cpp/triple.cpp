c8285eb94cbc541282047620d3aecdda4522cbde
Ray Toal <rtoal@lmu.edu>
2014-12-21 12:43:26 -0800

    C++ triple script

---
+++ cpp/triple.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  for (int c = 1; c <= 50; c++) {
+    for (int b = 1; b <= c; b++) {
+      for (int a = 1; a <= b; a++) {
+        if (a * a + b * b == c * c) {
+          cout << a << ", " << b << ", " << c << endl;
+        }
+      }
+    }
+  }
+  return 0;
+}
