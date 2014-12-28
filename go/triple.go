541a0e1a34692d9150fe088ba0c7b8c771c6d893
Ray Toal <rtoal@lmu.edu>
2014-12-28 02:43:28 -0800

    Fine tuning and adding some code

--- go/triple.go
+++ go/triple.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
     for c := 1; c <= 50; c++ {
-        for b := 1; b <= c; b++ {
-            for a := 1; a <= b; a++ {
+        for b := 1; b < c; b++ {
+            for a := 1; a < b; a++ {
                 if a * a + b * b == c * c {
                     fmt.Printf("%d, %d, %d\n", a, b, c)
                 }
