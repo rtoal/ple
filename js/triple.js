be1371054226371625262fa91bfda1382f1de373
Ray Toal <rtoal@lmu.edu>
2014-12-31 21:33:33 -0800

    Make upper bound for triple.js exclusive

--- js/triple.js
+++ js/triple.js
@@ -1,6 +1,6 @@
 for (var c = 1; c <= 50; c++) {
-    for (var b = 1; b <= c; b++) {
-        for (var a = 1; a <= b; a++) {
+    for (var b = 1; b < c; b++) {
+        for (var a = 1; a < b; a++) {
             if (a * a + b * b == c * c) {
                 console.log('%d, %d, %d', a, b, c);
             }
