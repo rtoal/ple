0d5c45e8cbbd76b0d4290d1940b8f9aa3de22aae
Ray Toal <rtoal@lmu.edu>
2015-01-03 22:44:02 -0800

    Various improvements

--- js/primitives_and_references.js
+++ js/primitives_and_references.js
@@ -1,5 +1,5 @@
-var a = {x: 3, y: 5}; // creates an object
-var b = a.y;
-var c = null;
-var d = {x: 3, y: 5}; // creates an object
-var e = d;            // does not create
+var a = {x: 3, y: 5};  // creates an object
+var b = a.y;           // simply puts 5 into b
+var c = null;          // simply puts null into c
+var d = {x: 3, y: 5};  // creates an object
+var e = d;             // does not create an object
