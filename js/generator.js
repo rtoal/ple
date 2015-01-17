3bb34d51ccfa0d2d0b148753dd10fc26fc78b2b0
Ray Toal <rtoal@lmu.edu>
2015-01-16 21:33:10 -0800

    Cleanups

--- js/generator.js
+++ js/generator.js
@@ -1,12 +1,11 @@
-function makeSquareGenerator() {
+var nextSquare = (function () {
   var previous = -1;
   return function () {
     previous++;
     return previous * previous;
   }
-}
+}());
 
-var nextSquare = makeSquareGenerator();
 console.log(nextSquare());    // prints 0
 console.log(nextSquare());    // prints 1
 console.log(nextSquare());    // prints 4
