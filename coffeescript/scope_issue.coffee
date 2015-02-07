c36cba4fedcd511ead8a1709df4358a9eb5f482e
Ray Toal <rtoal@lmu.edu>
2015-02-07 09:57:20 -0800

    Better explanatory comments re: scope

--- coffeescript/scope_issue.coffee
+++ coffeescript/scope_issue.coffee
@@ -1,9 +1,8 @@
 [a, b, c] = [0, 1, 2]
 f = () ->
-  a = 100    # Global because there's an a out there
-  b = 200    # Global because there's an a out there
-  d = 300    # Local because there's no d out there
-  e = 400    # Local because there's no e out there
+  a = 100    # Overwrites global: there's an a out there
+  b = 200    # Overwrites global: there's a b out there
+  d = 300    # Local, because there's no d out there
 f()
 console.log '%d %d %d', a, b, c
 try
--- js/scopes_and_var.js
+++ js/scopes_and_var.js
@@ -1,13 +1,13 @@
 var a = 0, b = 1, c = 2;
 (function () {
   var a = 100;   // Local declaration, global a is safe!
-  b = 200;       // Forgot var, destroyed global b
-  d = 300;       // Forgot var, created global d
+  b = 200;       // Forgot var, overwrites global b
+  d = 300;       // Forgot var, creates global d (oh my!)
   var e = 400;   // Local, will not exist after return
 }());
 console.log('%d %d %d %d', a, b, c, d);
 try {
   console.log(e);
-} catch (re) {
+} catch (error) {
   console.log('e is not defined out here');
 }
