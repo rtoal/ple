e025d754d7404cd9d36a6d8ada4570ae1c1056fe
Ray Toal <rtoal@lmu.edu>
2015-02-15 21:36:09 -0800

    Improvements to existing JavaScript scripts

--- js/exploring_parameters.js
+++ js/exploring_parameters.js
@@ -1,11 +1,11 @@
-var g = 100;          // Global
+var g = 100;          // Outside of f but visible to it
 var x = 200;          // Going to be passed to q
 var y = [1,2,3];      // Going to be passed to b
 var z = [2,3,4];      // Going to be passed to c
 
 function f(a, b, c) {
-  console.log(g);     // Yes, we can see globals -> 100
-  g = 300;            // We can even change globals
+  console.log(g);     // Yes, we can see enclosing scope -> 100
+  g = 300;            // We can even change vars in enclosing scope
   console.log(g);     // See it changed -> 300
   a = 400;            // We can write to the parameter
   console.log(x);     // But the argument DID NOT change -> 200
--- js/function_examples.js
+++ js/function_examples.js
@@ -1,8 +1,6 @@
-var assert = require('assert');
-
 // We don't have to name functions to call them
 var y = (function (x) {return x + 5})(10);
-assert(y === 15);
+console.log(y);                       // 15
 
 // We can assign function values to variables
 var square = function (x) {return x * x};
@@ -11,21 +9,21 @@ var lessThanTen = function (x) {return x < 10};
 var twice = function (f, x) {return f(f(x))};
 
 // We can pass function values by name, or anonymously
-assert(twice(square, -3) === 81);
-assert(twice(function (x) {return x + 1}, 5) === 7);
+console.log(twice(square, -3));                     // 81
+console.log(twice(function (x) {return x + 1}, 5)); // 7
 
 // We can create and return new functions on the fly
 function compose(f, g) {
   return function (x) {return f(g(x))};
 }
 var isOddWhenSquared = compose(odd, square);
-assert(isOddWhenSquared(7) === true);
-assert(isOddWhenSquared(0) === false);
+console.log(isOddWhenSquared(7));     // true
+console.log(isOddWhenSquared(0));     // false
 
 // Several built-in array functions accept functions
 var a = [9, 7, 4, -1, 8];
-assert(a.every(odd) === false);
-assert(a.some(odd) === true);
-assert(a.every(lessThanTen) === true);
-assert.deepEqual(a.filter(odd), [9, 7, -1]);
-assert.deepEqual(a.map(square), [81, 49, 16, 1, 64]);
+console.log(a.every(odd));            // false
+console.log(a.some(odd));             // true
+console.log(a.every(lessThanTen));    // true
+console.log(a.filter(odd));           // [9, 7, -1]
+console.log(a.map(square));           // [81, 49, 16, 1, 64]
