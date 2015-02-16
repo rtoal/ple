c47df03348a14b9336288f2f10687365b128b66d
Ray Toal <rtoal@lmu.edu>
2015-02-15 21:36:36 -0800

    First JS module example

---
+++ js/unique_string_demo.js
@@ -0,0 +1,6 @@
+var generateString = require('./string_generator.js');
+
+console.log(generateString('v'));   // v1
+console.log(generateString('v'));   // v2
+console.log(generateString('f'));   // f1
+console.log(generateString('v'));   // v3
