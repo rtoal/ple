d8b6b9fc5c1e905a7ae5499b70d32e1d972b25b0
Ray Toal <rtoal@lmu.edu>
2015-02-15 21:37:38 -0800

    DOM experiments

---
+++ js/button_error.js
@@ -0,0 +1,7 @@
+// Rookie mistake: alerts 10 for every button.
+for (var i = 0; i < 10; i++) {
+    button = document.createElement("button");
+    button.innerHTML = i;
+    button.onclick = function () {alert(i);}
+    document.body.appendChild(button);
+}
