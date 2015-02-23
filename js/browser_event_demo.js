d8fd7f78ba3504551ca691816b3231b9ee8057d0
Ray Toal <rtoal@lmu.edu>
2015-02-22 21:31:16 -0800

    Sketching is better than a dumb button example

--- js/browser_event_demo.js
+++ js/browser_event_demo.js
@@ -1,14 +1,20 @@
-var button = document.createElement('button');
-var number = document.createElement('div')
-button.innerHTML = 'Hello';
-button.onmouseover = function () {
-    button.style.backgroundColor = 'green';
-}
-button.onmouseout = function () {
-    button.style.backgroundColor = 'white';
-}
-button.onclick = function () {
-    number.innerHTML = Math.random();
-}
-document.body.appendChild(button);
-document.body.appendChild(number);
+(function () {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    var drawing = false;
+    canvas.style.border = '2px solid green';
+    canvas.onmousedown = function (e) {
+        drawing = true;
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+    canvas.onmousemove = function (e) {
+        if (drawing) {
+            ctx.lineTo(e.clientX, e.clientY);
+            ctx.stroke();
+        }
+    }
+    canvas.onmouseup = canvas.onmouseout = function () {
+        drawing = false;
+    }
+    document.body.appendChild(canvas);
+}());
