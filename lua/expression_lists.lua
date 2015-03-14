aca31ecb31c7ce174509d2ca1efaf73d6c52435d
Ray Toal <rtoal@lmu.edu>
2015-03-14 14:22:39 -0700

    Clarification in comments

--- lua/expression_lists.lua
+++ lua/expression_lists.lua
@@ -2,6 +2,6 @@ function three()
   return 5, 6, 7
 end
 
-a, b = three()            -- 5, 6
-c, d, e, f = three()      -- 5, 6, 7, nil
-g, h, i = 4, three()      -- 4, 5, 6
+a, b = three()          -- a=5, b=6
+c, d, e, f = three()    -- c=5, d=6, e=7, f=nil
+g, h, i = 4, three()    -- g=4, h=5, i=6
