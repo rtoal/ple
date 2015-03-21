8a5acacb234447bcd44055378d03abab03163d97
Ray Toal <rtoal@lmu.edu>
2015-03-20 21:55:34 -0700

    Update triple.rs to Rust 1.0

--- rust/triple.rs
+++ rust/triple.rs
@@ -1,7 +1,7 @@
 fn main() {
-    for c in range(1i, 51) {
-        for b in range(1, c) {
-            for a in range(1, b) {
+    for c in (1 .. 51) {
+        for b in (1 .. c) {
+            for a in (1 .. b) {
                 if a * a + b * b == c * c {
                     println!("{}, {}, {}", a, b, c)
                 }
