17eaebd9f3c4e17c74d6f4fa34cb0df5b548553e
Alex Schneider <aschne19@lion.lmu.edu>
2015-03-21 22:21:33 -0700

    Fix minor bug

--- rust/string_generator.rs
+++ rust/string_generator.rs
@@ -25,12 +25,4 @@ pub mod string_generator {
     }
 }
 
-fn main() {
-    let mut gen = string_generator::generator();
-    println!("{}", gen.generate("abc"));
-    println!("{}", gen.generate("abc"));
-    println!("{}", gen.generate("def"));
-    println!("{}", gen.generate("abc"));
-    println!("{}", gen.generate("def"));
-    println!("{}", gen.generate("ghi"));
-}
+
