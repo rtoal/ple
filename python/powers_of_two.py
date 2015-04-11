a56108990e2cda8694f7b5c4fe3c615966c4cd6c
Ray Toal <rtoal@lmu.edu>
2015-04-11 11:25:43 -0700

    Use asserts instead of prints

--- python/powers_of_two.py
+++ python/powers_of_two.py
@@ -9,9 +9,9 @@ for i in powers_of_two(70):
     print(i)
 
 # Explore the mechanism
-print(type(powers_of_two))  # <class 'function'>
 g = powers_of_two(100)
-print(type(g))              # <class 'generator'>
-print(g.__next__())         # 1
-print(g.__next__())         # 2
-print(next(g))              # 4
+assert(str(type(powers_of_two)) == "<class 'function'>")
+assert(str(type(g)) == "<class 'generator'>")
+assert(g.__next__() == 1)
+assert(g.__next__() == 2)
+assert(next(g) == 4)
