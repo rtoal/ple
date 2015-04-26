bbafaa720e80d409ab988b6dd75c71590c2669ad
Ray Toal <rtoal@lmu.edu>
2015-04-25 18:06:22 -0700

    Yikes there was a Python 2 script in there

--- python/wordcount.py
+++ python/wordcount.py
@@ -4,5 +4,6 @@ counts = {}
 for line in sys.stdin:
     for word in re.findall(r'[A-Za-z\']+', line.lower()):
         counts[word] = counts.get(word, 0) + 1
-for word, count in sorted(counts.iteritems()):
-    print word, count
+
+for word, count in sorted(counts.items()):
+    print(word, count, sep=' ')
