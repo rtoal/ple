371cddd5830d114f856b7dfd276685417a26e728
Ray Toal <rtoal@lmu.edu>
2015-04-26 21:08:09 -0700

    Refactor Python scope examples

--- python/scope.py
+++ python/scope.py
@@ -1,19 +1,11 @@
 x, y, z = 1, 2, 3   # Three global variables
 
 def f(x):
-    x = 4       # x is local because it is a parameter
-    y = 5       # makes a local y, shadows global y
-    w = 6       # makes a local w
+    x = 4           # parameters are local
+    y = 5           # local y, shadows global y
+    w = 6           # makes a local w
     global z
-    z = 7       # overwrites global z
-
-def g():
-    print(v)    # ERROR: used before assign
-    v = 8       # new local, declared too late
+    z = 7           # overwrites global z
 
 f(10)
-print('{}, {}, {}'.format(x, y, z))   # 1, 2, 7
-try:
-    g()
-except Exception, e:
-    print(e)
+assert(x == 1 and y == 2 and z == 7)
