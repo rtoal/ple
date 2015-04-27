e12c0efd2c2d2d1b8cff07438416eb549886d0c9
Ray Toal <rtoal@lmu.edu>
2015-04-26 21:08:44 -0700

    Simplify Python anagram script

--- python/anagrams.py
+++ python/anagrams.py
@@ -1,15 +1,13 @@
 import sys
 
-def swap(a, i, j):
-    a[i], a[j] = a[j], a[i]
-
 def generatePermutations(n, a):
     if n == 0:
-        print ''.join(a)
+        print(''.join(a))
     else:
         for i in range(n+1):
             generatePermutations(n-1, a)
-            swap(a, i if n % 2 == 0 else 0, n)
+            swap_index = i if n % 2 == 0 else 0
+            a[swap_index], a[n] = a[n], a[swap_index]
 
 if len(sys.argv) != 2:
     sys.stderr.write('Exactly one argument is required\n')
