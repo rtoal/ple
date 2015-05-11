d43c12bf132b8e6bb708b2f481c21700b959c6ab
Ray Toal <rtoal@lmu.edu>
2015-05-11 00:29:56 -0700

    The __init__ method is inherited

--- python/animals.py
+++ python/animals.py
@@ -1,28 +1,18 @@
 class Animal(object):
     def __init__(self, name):
         self.name = name
-
     def speak(self):
-        print self.name, 'says', self.sound()
+        print(self.name, 'says', self.sound())
 
 class Cow(Animal):
-    def __init__(self, name):
-        super(Cow, self).__init__(name)
-
     def sound(self):
         return 'moooo'
 
 class Horse(Animal):
-    def __init__(self, name):
-        super(Horse, self).__init__(name)
-
     def sound(self):
         return 'neigh'
 
 class Sheep(Animal):
-    def __init__(self, name):
-        super(Sheep, self).__init__(name)
-
     def sound(self):
         return 'baaaa'
 
