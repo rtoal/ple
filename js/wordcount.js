27d3f8b296739a5da0b5b313ee240b86baffa644
Ray Toal <rtoal@lmu.edu>
2015-05-10 21:58:01 -0700

    Improve JS wordcount

--- js/wordcount.js
+++ js/wordcount.js
@@ -6,9 +6,7 @@ process.stdin.setEncoding('utf8');
 
 process.stdin.pipe(split()).on('data', function (line) {
   (line.toLowerCase().match(/[a-z\']+/g) || []).forEach(
-    function (word) {
-      counts[word] = (counts[word] || 0) + 1;
-    }
+    function (word) {counts[word] = (counts[word] || 0) + 1;}
   );
 });
 
