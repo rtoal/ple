1c1a206dd5b1f64779591c0d7afd11c4dd16f998
Ray Toal <rtoal@lmu.edu>
2015-05-17 14:09:43 -0700

    Better comments in Lua scripts

--- lua/expression_lists.lua
+++ lua/expression_lists.lua
@@ -1,7 +1,7 @@
-function three()
+function computeThreeThings()
   return 5, 6, 7
 end
 
-a, b = three()          -- a=5, b=6
-c, d, e, f = three()    -- c=5, d=6, e=7, f=nil
-g, h, i = 4, three()    -- g=4, h=5, i=6
+a, b = computeThreeThings()          -- a=5, b=6
+c, d, e, f = computeThreeThings()    -- c=5, d=6, e=7, f=nil
+g, h, i = 4, computeThreeThings()    -- g=4, h=5, i=6
--- lua/locals.lua
+++ lua/locals.lua
@@ -1,7 +1,7 @@
 x = 1
 do
-  print(x)           -- 1          (global x)
-  local x = x + 2    -- 1 + 2 = 3  (global x)
-  print(x)           -- 3          (NOW its the local x)
+  print(x)           -- 1          (refers to global x)
+  local x = x + 2    -- 1 + 2 = 3  (also refers to global x!)
+  print(x)           -- 3          (NOW x refers to the local)
 end
 print(x)             -- back to 1  (in global scope again)
