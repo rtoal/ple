c798a88a298ec879944d900dda60f255e939f605
Ray Toal <rtoal@lmu.edu>
2015-05-17 15:06:55 -0700

    Illustrate "non-exclusive ranges" with upto

--- ruby/triple.rb
+++ ruby/triple.rb
@@ -1,6 +1,6 @@
 1.upto(50) do |c|
-  1.upto(c) do |b|
-    1.upto(b) do |a|
+  1.upto(c - 1) do |b|
+    1.upto(b - 1) do |a|
       puts "#{a}, #{b}, #{c}" if a * a + b * b == c * c
     end
   end
