70f16d317e51a6d3014b3be6a957c99b005f183c
Ray Toal <rtoal@lmu.edu>
2015-05-23 11:06:37 -0700

    Standard formatting

--- js/browser_event_demo.js
+++ js/browser_event_demo.js
@@ -1,20 +1,20 @@
 (function () {
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    var drawing = false;
-    canvas.style.border = '2px solid green';
-    canvas.onmousedown = function (e) {
-        drawing = true;
-        ctx.moveTo(e.clientX, e.clientY);
+  var canvas = document.createElement('canvas');
+  var ctx = canvas.getContext('2d');
+  var drawing = false;
+  canvas.style.border = '2px solid green';
+  canvas.onmousedown = function (e) {
+    drawing = true;
+    ctx.moveTo(e.clientX, e.clientY);
+  };
+  canvas.onmousemove = function (e) {
+    if (drawing) {
+      ctx.lineTo(e.clientX, e.clientY);
+      ctx.stroke();
     }
-    canvas.onmousemove = function (e) {
-        if (drawing) {
-            ctx.lineTo(e.clientX, e.clientY);
-            ctx.stroke();
-        }
-    }
-    canvas.onmouseup = canvas.onmouseout = function () {
-        drawing = false;
-    }
-    document.body.appendChild(canvas);
+  };
+  canvas.onmouseup = canvas.onmouseout = function () {
+    drawing = false;
+  };
+  document.body.appendChild(canvas);
 }());
