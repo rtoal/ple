b83ea3d1e32a1572ddb76c39f822adec454c38d9
Ray Toal <rtoal@lmu.edu>
2015-05-30 13:15:50 -0700

    Tidy up some Ruby scripts

--- ruby/block_scope.rb
+++ ruby/block_scope.rb
@@ -1,11 +1,11 @@
 def main
-    x = 100                              # x is a local var of main
-    [1,2,3].map{|x| x * x}               # a new block-local x
-    puts x                               # outer x unchanged at 100
-    [1,2,3].each{|y| x = x + y}          # block can mutate outer x
-    puts x                               # x mutated to 106
-    [1,2,3].each{|y; x| x = (x||0) + y}  # ; makes new x in block
-    puts x                               # x unchanged at 106
+  x = 100                              # x is a local var of main
+  [1,2,3].map{|x| x * x}               # a new block-local x
+  puts x                               # outer x unchanged at 100
+  [1,2,3].each{|y| x = x + y}          # block can mutate outer x
+  puts x                               # x mutated to 106
+  [1,2,3].each{|y; x| x = (x||0) + y}  # ; makes new x in block
+  puts x                               # x unchanged at 106
 end
 
 main
--- ruby/procs_and_lambdas.rb
+++ ruby/procs_and_lambdas.rb
@@ -3,25 +3,23 @@ plusThree = Proc.new {|x| x + 3}
 plusFour = lambda {|x| x + 4}
 plusFive = -> (x) {x + 5}
 
-# Basic Information
+# How do we call them? What are they? Regular or lambda?
 p [plusTwo.call(10), plusTwo.class, plusTwo.lambda?]
 p [plusThree.call(10), plusThree.class, plusThree.lambda?]
 p [plusFour.call(10), plusThree.class, plusFour.lambda?]
 p [plusFive.call(10), plusThree.class, plusFive.lambda?]
 
-# Argument checking
-plusTwo.call(100, 200, 300)     # No problem, yikes
+plusTwo.call(100, 200, 300)     # Extra args? No problem!
 begin
-  plusFour.call(100, 200, 300)  # Will raise an error
+  plusFour.call(100, 200, 300)  # But it's a problem here!
 rescue ArgumentError
   puts 'Rescued'                # Message will be output
 end
 
-# Passing procs
 a = [10, 20, 30, 40, 50]
-p a.map(&plusTwo)
+p a.map(&plusTwo)               # All passed the same way
 p a.map(&plusThree)
 p a.map(&plusFour)
 p a.map(&plusFive)
-p a.map(&(->(x) {x * 10}))      # anonymous
-p a.map &->(x) {x * 10}         # a crazier way to write it
+p a.map(&(->(x) {x * 10}))      # anonymous procs!
+p a.map &->(x) {x * 10}         # Parens optional here too
