efe88740164d57990310abbd208f6388994140f3
Ray Toal <rtoal@lmu.edu>
2015-06-20 14:37:47 -0700

    ES2015 time, woot

--- js/anagrams.js
+++ js/anagrams.js
@@ -1,15 +1,13 @@
 function swap(a, i, j) {
-  var saved = a[i];
-  a[i] = a[j];
-  a[j] = saved;
+  [a[i],a[j]] = [a[j],a[i]]
 }
 
-function generatePermutations(n, a) {
+function generatePermutations(a, n) {
   if (n === 0) {
     console.log(a.join(''));
   } else {
-    for (var i = 0; i <= n; i++) {
-      generatePermutations(n-1, a);
+    for (let i = 0; i <= n; i++) {
+      generatePermutations(a, n - 1);
       swap(a, n % 2 === 0 ? i : 0, n);
     }
   }
@@ -19,5 +17,5 @@ if (process.argv.length !== 3) {
   console.error('Exactly one argument is required');
   process.exit(1);
 }
-var word = process.argv[2];
-generatePermutations(word.length-1, word.split(''));
+let word = process.argv[2];
+generatePermutations(word.split(''), word.length - 1);
--- js/wordcount.js
+++ js/wordcount.js
@@ -1,17 +1,17 @@
-var split = require('split');
+let split = require('split');
 
-var counts = Object.create(null);
+let counts = Object.create(null);
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.pipe(split()).on('data', function (line) {
-  (line.toLowerCase().match(/[a-z\']+/g) || []).forEach(
-    function (word) {counts[word] = (counts[word] || 0) + 1;}
-  );
-});
+process.stdin.pipe(split()).on('data', line =>
+  (line.toLowerCase().match(/[a-z\']+/g) || []).forEach(word =>
+    counts[word] = (counts[word] || 0) + 1
+  )
+);
 
-process.stdin.on('end', function () {
-  Object.keys(counts).sort().forEach(function (word) {
-    console.log('%s %d', word, counts[word]);
-  });
-});
+process.stdin.on('end', () =>
+  Object.keys(counts).sort().forEach(word =>
+    console.log('%s %d', word, counts[word])
+  )
+);
