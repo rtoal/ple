460be3c56f7e399921e11cb9eafea6ad99b94f93
Ray Toal <rtoal@lmu.edu>
2015-06-28 21:23:38 -0700

    Reverse order of parameters

--- python/anagrams.py
+++ python/anagrams.py
@@ -1,11 +1,11 @@
 import sys
 
-def generatePermutations(n, a):
+def generatePermutations(a, n):
     if n == 0:
         print(''.join(a))
     else:
         for i in range(n+1):
-            generatePermutations(n-1, a)
+            generatePermutations(a, n-1)
             swap_index = i if n % 2 == 0 else 0
             a[swap_index], a[n] = a[n], a[swap_index]
 
@@ -13,4 +13,4 @@ if len(sys.argv) != 2:
     sys.stderr.write('Exactly one argument is required\n')
     sys.exit(1)
 word = sys.argv[1]
-generatePermutations(len(word)-1, list(word))
+generatePermutations(list(word), len(word)-1)
