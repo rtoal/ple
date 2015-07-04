c3fe2b8035eaacf264b67496d0c025427a7ba779
Ray Toal <rtoal@lmu.edu>
2015-07-03 17:24:20 -0700

    Reverse parameter order

--- lua/anagrams.lua
+++ lua/anagrams.lua
@@ -1,9 +1,9 @@
-function generatePermutations(n, a)
+function generatePermutations(a, n)
   if n == 0 then
     print(utf8.char(table.unpack(a)))
   else
     for i = 1, n do
-      generatePermutations(n-1, a)
+      generatePermutations(a, n-1)
       swapIndex = n % 2 == 0 and i or 1
       a[swapIndex], a[n] = a[n], a[swapIndex]
     end
@@ -15,4 +15,4 @@ if #arg ~= 1 then
   os.exit(1)
 end
 word = {utf8.codepoint(arg[1], 1, utf8.len(arg[1]))}
-generatePermutations(#word, word)
+generatePermutations(word, #word)
