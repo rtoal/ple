e925f6600be63ad2e51f73c439acdbde4115abdd
Ray Toal <rtoal@lmu.edu>
2015-08-17 23:57:56 -0700

    Updating JS stuff

--- js/exploring_parameters.js
+++ js/exploring_parameters.js
@@ -1,18 +1,20 @@
+import assert from 'assert';
+
 let g = 100;          // Outside of f but visible to it
 let x = 200;          // Going to be passed to a
 let y = [1,2,3];      // Going to be passed to b
 let z = [2,3,4];      // Going to be passed to c
 
 function f(a, b, c) {
-  console.log(g);     // Yes, we can see enclosing scope -> 100
+  assert(g === 100);  // Yes, we can see enclosing scope
   g = 300;            // We can even change vars in enclosing scope
-  console.log(g);     // See it changed -> 300
+  assert(g === 300);  // See it changed -> 300
   a = 400;            // We can write to the parameter
-  console.log(x);     // But the argument DID NOT change -> 200
+  assert(x === 200);  // But the argument DID NOT change
   b = 500;            // Change another parameter
-  console.log(y);     // Argument still intact -> [1,2,3]
-  c[1] = 600;         // But we CAN change through a reference!
-  console.log(z);     // See the change! -> [2,600,4]
+  assert.deepEqual(y, [1,2,3]);    // Argument still intact
+  c[1] = 600;                      // We CAN change via reference
+  assert.deepEqual(z, [2,600,4]);  // See the change!
 }
 
 f(x, y, z);
--- js/wordcount.js
+++ js/wordcount.js
@@ -1,16 +1,16 @@
 import split from 'split';
-import {XRegExp} from 'xregexp'
+import {XRegExp} from 'xregexp';
 
 let counts = Object.create(null);
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.pipe(split()).on('data', line =>
+process.stdin.pipe(split()).on('data', line => {
   let wordPattern = XRegExp("[\\p{L}']+", 'g');
   (line.toLowerCase().match(wordPattern) || []).forEach(word =>
     counts[word] = (counts[word] || 0) + 1
-  )
-);
+  );
+});
 
 process.stdin.on('end', () =>
   Object.keys(counts).sort().forEach(word =>
