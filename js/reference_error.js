5d13509f2f9dee27888e288d3f3801ad82f18270
Ray Toal <rtoal@lmu.edu>
2015-08-23 09:53:07 -0700

    Oh cool, node assert can check exception type

--- js/reference_error.js
+++ js/reference_error.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
 
 let p = {x: 3, y: 5}
-assert(p.z === undefined); // There's no p.z
-assert.throws(() => z);    // There's no global z
+assert(p.z === undefined);                    // There's no p.z
+assert.throws(() => z, ReferenceError);       // No variable z
+assert.throws(() => {z = 5}, ReferenceError); // No variable z
