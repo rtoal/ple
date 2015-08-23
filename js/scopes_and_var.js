03f0c156e5d1f70a0a10fbebc3b94cf73103d7e6
Ray Toal <rtoal@lmu.edu>
2015-08-23 10:35:22 -0700

    Woops, strict mode

--- js/scopes_and_var.js
+++ js/scopes_and_var.js
@@ -1,14 +1,11 @@
 // JavaScript illustration of local variables
 var a = 0, b = 1, c = 2;
-(function () {
-  var a = 100;   // Local declaration, global a is safe!
-  b = 200;       // Forgot var, overwrites global b
-  d = 300;       // Forgot var, creates global d (oh my!)
-  var e = 400;   // Local, will not exist after return
-}());
-console.log('%d %d %d %d', a, b, c, d);
-try {
-  console.log(e);
-} catch (error) {
-  console.log('e is not defined out here');
-}
+() => {
+  var a = 100;   // Local, shadows the global a
+  b = 200;       // Forgot var, overwrites global b!
+  var d = 300;   // Local, will not exist after return
+}();
+
+import assert from 'assert'
+assert.deepEqual([a,b,c], [0, 200, 2])
+assert.throws(() => d, ReferenceError)
