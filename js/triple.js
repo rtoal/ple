73c55365c6130d1ffe8c4b44442b972438364dfd
Ray Toal <rtoal@lmu.edu>
2015-08-30 21:41:37 -0700

    == in JS wut?

--- js/triple.js
+++ js/triple.js
@@ -1,7 +1,7 @@
 for (let c = 1; c <= 50; c++) {
   for (let b = 1; b < c; b++) {
     for (let a = 1; a < b; a++) {
-      if (a * a + b * b == c * c) {
+      if (a * a + b * b === c * c) {
         console.log('%d, %d, %d', a, b, c);
       }
     }
