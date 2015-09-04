78a23f92f1e465a62abbc42d25697dfc9bff4412
Ray Toal <rtoal@lmu.edu>
2015-09-04 10:09:02 -0700

    Julia code

--- julia/parametric_types.jl
+++ julia/parametric_types.jl
@@ -1,5 +1,7 @@
-@assert typeof([5,3] == Array{Int64,1}
-@assert typeof(8.75im == Complex{Float64}
-@assert typeof([] == Array{None, 1}
-@assert typeof([3, "abc"] == Array{Any, 1}
-@assert typeof([1 0; 0 1] == Array{Int64, 2}
+@assert typeof(0x5 // 0x22) == Rational{Uint8}
+@assert typeof(5 // 34) == Rational{Int64}
+@assert typeof(8.75im) == Complex{Float64}
+@assert typeof([5,3]) == Array{Int64,1}
+@assert typeof([3, "abc"]) == Array{Any, 1}
+@assert typeof([]) == Array{None, 1}
+@assert typeof([1 0; 0 1]) == Array{Int64, 2}
--- julia/scopes.jl
+++ julia/scopes.jl
@@ -1,15 +1,15 @@
-a, b, c = 1, 2, 3              # three globals
+a, b, c = 1, 2, 3       # three globals
 (function ()
-  a = 10                       # introduces local and shadows
-  global b = 20                # overwrites global
-  d = 10c + 10                 # new local, reading global
+  a = 10                # introduces local and shadows
+  global b = 20         # overwrites global
+  d = 10c + 10          # new local, reading global
   local e = 5
   while true
-    e = 50                     # outer e because marked local
-    f = 60                     # local to while loop!
+    e = 50              # outer e because marked local
+    f = 60              # local to while loop!
     break
   end
-  println((a,b,c,d,e))        # (10,20,3,40,50)
-  println(try f catch -1 end) # -1, because no f out here
+  @assert (a,b,c,d,e) == (10,20,3,40,50)
+  @assert (try f catch -1 end) == -1
 end)()
-println((a,b,c))              # (1,20,3)
+@assert (a,b,c) == (1,20,3)
