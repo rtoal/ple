efc441b6e0eb6ff48b487bf1d3bef87c087c236b
Rachel Rivera <rachelriv94@gmail.com>
2015-09-07 19:59:12 -0700

    tab fail

--- java/AlternativeWordCountApp.java
+++ java/AlternativeWordCountApp.java
@@ -7,11 +7,11 @@ import java.util.Arrays;
 public class AlternativeWordCountApp {
 
     public static void main(String[] args) {
-    	((new BufferedReader(new InputStreamReader(System.in)))
-    		.lines()
+        ((new BufferedReader(new InputStreamReader(System.in)))
+            .lines()
             .map(line -> line.split("[^a-zA-Z']+"))
-            .flatMap(Arrays::stream)	
-    		.collect(groupingBy(String::toLowerCase, TreeMap::new, counting())))
-    	.forEach((word, count) -> System.out.printf("%s %d\n", word, count));
+            .flatMap(Arrays::stream)    
+            .collect(groupingBy(String::toLowerCase, TreeMap::new, counting())))
+        .forEach((word, count) -> System.out.printf("%s %d\n", word, count));
     }
 }
