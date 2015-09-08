7213b682e7cb16b4fa3be35818845c80a12d19ad
Rachel Rivera <rachelriv94@gmail.com>
2015-09-07 20:03:07 -0700

    combine map and flat map

--- java/AlternativeWordCountApp.java
+++ java/AlternativeWordCountApp.java
@@ -9,8 +9,7 @@ public class AlternativeWordCountApp {
     public static void main(String[] args) {
         ((new BufferedReader(new InputStreamReader(System.in)))
             .lines()
-            .map(line -> line.split("[^a-zA-Z']+"))
-            .flatMap(Arrays::stream)    
+            .flatMap(line -> Arrays.stream(line.split("[^a-zA-Z']+")))    
             .collect(groupingBy(String::toLowerCase, TreeMap::new, counting())))
         .forEach((word, count) -> System.out.printf("%s %d\n", word, count));
     }
