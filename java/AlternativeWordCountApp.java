4696a17d9d1b8a401ac91c1ffb07dc68a8c2f064
Rachel Rivera <rachelriv94@gmail.com>
2015-09-07 20:18:51 -0700

    fix faulty logic in converting words to lower case

--- java/AlternativeWordCountApp.java
+++ java/AlternativeWordCountApp.java
@@ -3,14 +3,15 @@ import java.util.stream.Collectors;
 import java.io.*;
 import static java.util.stream.Collectors.*;
 import java.util.Arrays;
+import static java.util.function.*;
 
 public class AlternativeWordCountApp {
 
     public static void main(String[] args) {
         ((new BufferedReader(new InputStreamReader(System.in)))
             .lines()
-            .flatMap(line -> Arrays.stream(line.split("[^a-zA-Z']+")))    
-            .collect(groupingBy(String::toLowerCase, TreeMap::new, counting())))
+            .flatMap(line -> Arrays.stream(line.toLowerCase().split("[a-z']+")))    
+            .collect(groupingBy(identity(), TreeMap::new, counting())))
         .forEach((word, count) -> System.out.printf("%s %d\n", word, count));
     }
 }
