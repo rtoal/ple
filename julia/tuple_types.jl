e9cf5d3f92fee8d8deebb106ebff0cf31957d67c
Ray Toal <rtoal@lmu.edu>
2015-09-09 11:33:07 -0700

    More asserts for the Julia type scripts

--- julia/tuple_types.jl
+++ julia/tuple_types.jl
@@ -1,7 +1,7 @@
-t = (Uint8, Bool)
-@assert isa(t, Type)
+t = (Uint8, Bool)               # a tuple type
+@assert isa(t, Type)            # tuple types are types
 
-@assert isa((0x08, false), t)
+@assert isa((0x08, false), t)   # tuples of the right type belong
 @assert isa((0x7A, true), t)
-@assert !isa(0x25, t)
-@assert !isa(false, t)
+@assert !isa((3, "wrong"), t)   # tuples of the wrong type do not
+@assert !isa(0x25, t)           # non-tuples do not belong
--- julia/union_types.jl
+++ julia/union_types.jl
@@ -1,8 +1,8 @@
-u = Union(Uint8, Bool)
-@assert typeof(u) == UnionType
+u = Union(Uint8, Bool)            # A new type
+@assert typeof(u) == UnionType    # Julia has a type for all union types
 
-@assert isa(0x08, u)
-@assert isa(false, u)
+@assert isa(0x08, u)              # Uint8 values belong
+@assert isa(false, u)             # Boolean values belong
 @assert isa(true, u)
-@assert !isa(256, u)
-@assert !isa((0x08, true), u)
+@assert !isa(256, u)              # Not a Uint8, does not belong
+@assert !isa((0x08, true), u)     # Tuples do not belong
