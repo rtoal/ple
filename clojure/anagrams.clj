50c814f2aa93ee2f0e789f0696bdce6b4367c5c2
Ray Toal <rtoal@lmu.edu>
2015-09-13 22:15:42 -0700

    Change order of arguments in anagram script

--- clojure/anagrams.clj
+++ clojure/anagrams.clj
@@ -1,14 +1,14 @@
 (defn swap [a i j]
-   (assoc a j (a i) i (a j)))
+  (assoc a j (a i) i (a j)))
 
-(defn generatePermutations [n v]
+(defn generatePermutations [v n]
   (if (zero? n)
     (println (apply str v))
 
     (loop [i 0 a v]
       (if (<= i n)
         (do
-          (generatePermutations (dec n) a)
+          (generatePermutations a (dec n))
           (recur (inc i) (swap a (if (even? n) i 0) n)))))))
 
 (if (not= (count *command-line-args*) 1)
@@ -16,4 +16,4 @@
     (println "Exactly one argument is required")
     (System/exit 1))
   (let [word (-> *command-line-args* first vec)]
-    (generatePermutations (dec (count word)) word)))
+    (generatePermutations word (dec (count word)))))
