7af8a3fa7a9ead433c28d79e317f1ff9ecf6d8db
Ray Toal <rtoal@lmu.edu>
2015-09-20 14:17:44 -0700

    Improve unicode wordcount program to use readline

--- js/wordcount.js
+++ js/wordcount.js
@@ -1,18 +1,13 @@
-import split from 'split';
-import {XRegExp} from 'xregexp';
-
+let reader = require('readline').createInterface(process.stdin, null);
+let XRegExp = require('xregexp').XRegExp;
 let counts = Object.create(null);
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.pipe(split()).on('data', line => {
+reader.on('line', line => {
   let wordPattern = XRegExp("[\\p{L}']+", 'g');
   (line.toLowerCase().match(wordPattern) || []).forEach(word =>
     counts[word] = (counts[word] || 0) + 1
   );
-});
-
-process.stdin.on('end', () =>
+}).on('close', () =>
   Object.keys(counts).sort().forEach(word =>
     console.log('%s %d', word, counts[word])
   )
