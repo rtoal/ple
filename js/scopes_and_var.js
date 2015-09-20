e7cea403c64f1588bc3268d612b3153f6329c2a6
Ray Toal <rtoal@lmu.edu>
2015-09-20 14:39:28 -0700

    Another import -> require

--- js/scopes_and_var.js
+++ js/scopes_and_var.js
@@ -6,6 +6,6 @@ var a = 0, b = 1, c = 2;
   var d = 300;   // Local, will not exist after return
 }();
 
-import assert from 'assert'
+let assert = require('assert')
 assert.deepEqual([a,b,c], [0, 200, 2])
 assert.throws(() => d, ReferenceError)
