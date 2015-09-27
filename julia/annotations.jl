36f686416d9d82981fb655939a1fb071ba28200d
Ray Toal <rtoal@lmu.edu>
2015-09-27 15:48:38 -0700

    Julia type annotations example

---
+++ julia/annotations.jl
@@ -0,0 +1,9 @@
+(function()
+  local x::Int64 = 0x02
+  local y = 0x02
+  @assert typeof(x) == Int64  # The UInt8 was converted
+  @assert typeof(y) == UInt8  # No annotations, no coversion
+
+  # There's no conversion from ASCIIString to Int64
+  @assert isa(try x = "Oh no" catch (e) e end, Exception)
+end)()
