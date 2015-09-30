45322b2846076920a661c9aa1b9a4031379c34cf
Rachel Rivera <rachelriv94@gmail.com>
2015-09-30 12:55:21 -0700

    add scope example and fit WordCountApp.java into book

---
+++ java/Scope.java
@@ -0,0 +1,6 @@
+void f() {
+  int x = 1;
+  if (true) {
+    int x = 2;
+  }
+}
--- java/WordCountApp.java
+++ java/WordCountApp.java
@@ -2,18 +2,21 @@ import java.util.TreeMap;
 import java.util.Arrays;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.counting;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class WordCountApp {
 
     public static void main(String[] args) {
         ((new BufferedReader(new InputStreamReader(System.in)))
             .lines()
-            .flatMap(line -> Arrays.stream(line.split("[^a-zA-Z']+")))
-            .collect(groupingBy(String::toLowerCase, TreeMap::new, counting())))
-        .forEach((word, count) -> System.out.printf("%s %d\n", word, count));
+            .flatMap(line -> 
+                Arrays.stream(line.toLowerCase().split("[^a-z']+")))
+            .collect(Collectors.groupingBy(Function.identity(), 
+                                           TreeMap::new, 
+                                           Collectors.counting())))
+        .forEach((word, count) -> 
+            System.out.printf("%s %d\n", word, count));
     }
 
 }
