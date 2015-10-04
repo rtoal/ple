af02fe0eafc420cf603faea4ce7260706b6b0373
Ray Toal <rtoal@lmu.edu>
2015-10-04 14:27:52 -0700

    Get rid of a superfluous example

--- julia/union_types.jl
+++ julia/union_types.jl
@@ -6,4 +6,3 @@ u = Union{UInt8, Bool}          # A new type
 @assert isa(false, u)           # Boolean values belong
 @assert isa(true, u)
 @assert !isa(256, u)            # Not a UInt8, does not belong
-@assert !isa((0x08, true), u)   # Tuples do not belong
