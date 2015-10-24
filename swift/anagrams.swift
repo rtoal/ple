120c84ce69b84d50a4787b20b1ce01cbc0c5148a
Ray Toal <rtoal@lmu.edu>
2015-10-24 12:57:17 -0700

    Hey Swift has a built-in swap, who knew?

--- swift/anagrams.swift
+++ swift/anagrams.swift
@@ -1,19 +1,14 @@
 import Darwin
 
-func swap(index i: Int, with j: Int, inout ofArray a: [Character]) {
-    let saved = a[i]
-    a[i] = a[j]
-    a[j] = saved
-}
-
 func generatePermutations(inout a : [Character], upTo n: Int) {
     if n == 0 {
         print((a.map{"\($0)"}).joinWithSeparator(""))
     } else {
-        for i in 0...n {
+        for i in 0..<n {
             generatePermutations(&a, upTo: n-1)
-            swap(index: (n % 2 == 0 ? i : 0), with: n, ofArray: &a)
+            swap(&a[n % 2 == 0 ? i : 0], &a[n])
         }
+        generatePermutations(&a, upTo: n-1)
     }
 }
 
@@ -21,7 +16,6 @@ if Process.arguments.count != 2 {
     fputs("Exactly one argument is required\n", __stderrp)
     exit(1)
 }
-
-var word = Process.arguments[1]
+let word = Process.arguments[1]
 var charArray = word.characters.map {$0}
 generatePermutations(&charArray, upTo: charArray.count-1)
