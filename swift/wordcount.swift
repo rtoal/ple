09841c115b61e12d07445edac7aeb79368711ab0
Ray Toal <rtoal@lmu.edu>
2015-10-31 15:28:01 -0700

    Better to use a guard statement than explicitly unwrap

--- swift/wordcount.swift
+++ swift/wordcount.swift
@@ -3,12 +3,13 @@ import Foundation
 var counts = [String: Int]()
 
 while let line = readLine()?.lowercaseString {
-    let indices = line.characters.indices
-    line.enumerateSubstringsInRange(indices, options: .ByWords) {
-        word, _, _, _ in counts[word!] = (counts[word!] ?? 0) + 1
+    let range = line.characters.indices
+    line.enumerateSubstringsInRange(range, options: .ByWords) {w,_,_,_ in
+        guard let word = w else {return}
+        counts[word] = (counts[word] ?? 0) + 1
     }
 }
 
-for (word, count) in (counts.sort { $0.0 < $1.0 }) {
+for (word, count) in (counts.sort {$0.0 < $1.0}) {
     print("\(word) \(count)")
 }
