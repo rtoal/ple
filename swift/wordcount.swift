a7e6c58200eac21770d0c24c0eb26bf2cc8b993d
Ray Toal <rtoal@lmu.edu>
2015-10-30 20:43:34 -0700

    Better Swift word counter

--- swift/wordcount.swift
+++ swift/wordcount.swift
@@ -1,18 +1,12 @@
 import Foundation
 
-// Should be fixed: This reads in all of standard input
-let standardInput = NSFileHandle.fileHandleWithStandardInput()
-let input = standardInput.availableData
-let text = String(data: input, encoding: NSUTF8StringEncoding)!
-
 var counts = [String: Int]()
 
-func isWordChar (c: Character) -> Bool {
-    return "abcdefghijklmnopqrstuvwxyz'".rangeOfString("\(c)") != nil
-}
-
-for word in (text.lowercaseString.characters.split{!isWordChar($0)}.map(String.init)) {
-    counts[word] = (counts[word] ?? 0) + 1
+while let line = readLine()?.lowercaseString {
+    let indices = line.characters.indices
+    line.enumerateSubstringsInRange(indices, options: .ByWords) {
+        word, _, _, _ in counts[word!] = (counts[word!] ?? 0) + 1
+    }
 }
 
 for (word, count) in (counts.sort { $0.0 < $1.0 }) {
