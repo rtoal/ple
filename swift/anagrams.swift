62ebef8fcb804747085fea84d8315c26c06d4c1e
Ray Toal <rtoal@lmu.edu>
2015-10-31 12:20:43 -0700

    No closures needed for string and character array conversions

--- swift/anagrams.swift
+++ swift/anagrams.swift
@@ -2,7 +2,7 @@ import Darwin
 
 func generatePermutations(inout a : [Character], upTo n: Int) {
     if n == 0 {
-        print((a.map{"\($0)"}).joinWithSeparator(""))
+        print(String(a))
     } else {
         for i in 0..<n {
             generatePermutations(&a, upTo: n-1)
@@ -17,5 +17,5 @@ if Process.arguments.count != 2 {
     exit(1)
 }
 let word = Process.arguments[1]
-var charArray = word.characters.map {$0}
+var charArray = Array(word.characters)
 generatePermutations(&charArray, upTo: charArray.count-1)
