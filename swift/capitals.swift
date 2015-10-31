bde404a26a81b008af2166efe442beed1adfeb49
Ray Toal <rtoal@lmu.edu>
2015-10-31 12:36:06 -0700

    New Swift snippets

---
+++ swift/capitals.swift
@@ -0,0 +1,12 @@
+let capitals = [
+  "ME": "Augusta",
+  "VT": "Montpelier",
+  "NH": "Concord",
+  "MA": "Boston",
+  "RI": "Providence",
+  "CT": "Hartford",
+]
+
+for (state, capital) in capitals {
+  print("The capital of \(state) is \(capital).")
+}
