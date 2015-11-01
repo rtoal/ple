3c56cf28543d39013285591c16a148a5bd4a300d
Ray Toal <rtoal@lmu.edu>
2015-11-01 15:42:53 -0800

    Use char arrays and type hints

--- clojure/anagrams-mutable.clj
+++ clojure/anagrams-mutable.clj
@@ -2,17 +2,17 @@
 ; Highly non-idiomatic. Should not be used. EXPERIMENTAL.
 ; Also it is ridiculously slow. Does anyone know why?
 
-(defn generatePermutations [a n]
+(defn generatePermutations [^chars a n]
   (if (zero? n)
-    (println (apply str a))
+    ();(println (apply str a))
     (doseq [i (range 0 (inc n))]
       (generatePermutations a (dec n))
       (let [j (if (even? n) i 0) oldn (aget a n) oldj (aget a j)]
-        (aset a n oldj) (aset a j oldn)))))
+        (aset-char a n oldj) (aset-char a j oldn)))))
 
 (if (not= (count *command-line-args*) 1)
   (do
     (println "Exactly one argument is required")
     (System/exit 1))
-  (let [word (-> *command-line-args* first vec to-array)]
+  (let [word (-> *command-line-args* first vec char-array)]
     (time (generatePermutations word (dec (count word))))))
