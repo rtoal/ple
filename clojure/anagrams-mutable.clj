a6788f11c5bceb51448f779d1e045cc8f47589d4
Ray Toal <rtoal@lmu.edu>
2015-11-01 12:36:00 -0800

    An experiment

---
+++ clojure/anagrams-mutable.clj
@@ -0,0 +1,18 @@
+; An attempt to use in-place array mutation in Clojure
+; Highly non-idiomatic. Should not be used. EXPERIMENTAL.
+; Also it is ridiculously slow. Does anyone know why?
+
+(defn generatePermutations [a n]
+  (if (zero? n)
+    (println (apply str a))
+    (doseq [i (range 0 (inc n))]
+      (generatePermutations a (dec n))
+      (let [j (if (even? n) i 0) oldn (aget a n) oldj (aget a j)]
+        (aset a n oldj) (aset a j oldn)))))
+
+(if (not= (count *command-line-args*) 1)
+  (do
+    (println "Exactly one argument is required")
+    (System/exit 1))
+  (let [word (-> *command-line-args* first vec to-array)]
+    (time (generatePermutations word (dec (count word))))))
