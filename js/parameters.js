a1e9d4f44b5a61579355fdd47230063085169fcb
Ray Toal <rtoal@lmu.edu>
2015-11-27 14:40:38 -0800

    Use deepStrictEqual

--- js/parameters.js
+++ js/parameters.js
@@ -3,8 +3,8 @@ let assert = require('assert');
 function f(x, y) {return [x, y]}
 function g(x, ...y) {return [x, y]}
 
-assert.deepEqual(f(1), [1, undefined]);     // too few arguments
-assert.deepEqual(f(1, 2, 3), [1, 2]);       // too many arguments
-assert.deepEqual(g(1, 2, 3), [1, [2, 3]]);  // later arguments packed
+assert.deepStrictEqual(f(1), [1, undefined]);     // too few args
+assert.deepStrictEqual(f(1, 2, 3), [1, 2]);       // too many args
+assert.deepStrictEqual(g(1, 2, 3), [1, [2, 3]]);  // args packed
 var args = [1, 2]
-assert.deepEqual(f(...args), [1, 2]);       // args unpacked to x and y
+assert.deepStrictEqual(f(...args), [1, 2]);       // args unpacked
