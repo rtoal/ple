d0f45c9748e33f54fac44a8b223ab84e1609c62f
Ray Toal <rtoal@lmu.edu>
2015-11-27 21:56:22 -0800

    Better explanations in Julia scripts

--- julia/isa.jl
+++ julia/isa.jl
@@ -1,4 +1,4 @@
-@assert typeof(88) == Int64
-@assert isa(88, Signed)
+@assert typeof(88) == Int64    # because Int64 is concrete
+@assert isa(88, Signed)        # isa works for all types
 @assert isa(88, Integer)
 @assert isa(88, Real) and isa(88, Number)
