225dd1cb4783b3d8cb9b2984ad6631064aa39a5e
Ray Toal <rtoal@lmu.edu>
2015-11-27 21:54:36 -0800

    Use Array.from to get map keys

--- js/wordcount.js
+++ js/wordcount.js
@@ -1,14 +1,14 @@
 let reader = require('readline').createInterface(process.stdin, null);
 let XRegExp = require('xregexp').XRegExp;
-let counts = Object.create(null);
+let counts = new Map();
 
 reader.on('line', line => {
   let wordPattern = XRegExp("[\\p{L}']+", 'g');
   (line.toLowerCase().match(wordPattern) || []).forEach(word =>
-    counts[word] = (counts[word] || 0) + 1
+    counts.set(word, (counts.get(word) || 0) + 1)
   );
 }).on('close', () =>
-  Object.keys(counts).sort().forEach(word =>
-    console.log('%s %d', word, counts[word])
+  Array.from(counts.keys()).sort().forEach(word =>
+    console.log('%s %d', word, counts.get(word))
   )
 );
--- js/wordcount_ascii.js
+++ js/wordcount_ascii.js
@@ -1,13 +1,13 @@
 let readline = require('readline');
 let reader = readline.createInterface(process.stdin, null);
-let counts = Object.create(null);
+let counts = new Map();
 
 reader.on('line', line =>
   (line.toLowerCase().match(/[a-z']+/g) || []).forEach(word =>
-    counts[word] = (counts[word] || 0) + 1
+    counts.set(word, (counts.get(word) || 0) + 1)
   )
 ).on('close', () =>
-  Object.keys(counts).sort().forEach(word =>
-    console.log('%s %d', word, counts[word])
+  Array.from(counts.keys()).sort().forEach(word =>
+    console.log('%s %d', word, counts.get(word))
   )
 );
