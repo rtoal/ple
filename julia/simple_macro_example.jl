5404e9a64c5bd099e5db6e5fedcc4030f98ed5bb
Ray Toal <rtoal@lmu.edu>
2015-11-29 18:23:40 -0800

    Clean up some of the Julia examples

--- julia/simple_macro_example.jl
+++ julia/simple_macro_example.jl
@@ -1,14 +1,14 @@
-macro addTwoToX()
-  println("hello");
-  :(x += 2)
+macro tenTimesX()
+  println("hello");     # executed at compile time
+  :(x * 10)             # expression returned at compile time
 end
 
 function f()
   x = 2;
-  @addTwoToX()
-end
+  @tenTimesX()          # replaced with x*10 at compile time
+end                     # when called, f executes x=2; x*10
 
-@assert f() == 4
-@assert f() == 4
-@assert f() == 4
-@assert f() == 4
+@assert f() == 20
+@assert f() == 20
+@assert f() == 20
+@assert f() == 20
