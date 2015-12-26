a2997ef49c854b83c15db5817eea8d225d6fbc98
Ray Toal <rtoal@lmu.edu>
2015-12-26 12:08:01 -0800

    Parse time, not compile time

--- julia/hygiene_example.jl
+++ julia/hygiene_example.jl
@@ -5,6 +5,6 @@ end
 (function ()
   x = 2;
   y = 3;                     # Will this y be changed?
-  z = @tenTimesX()           # Changed during expansion?
+  z = @tenTimesX()           # Changed to 10 perhaps?
   @assert y == 3 && z == 20  # No, y will not change
 end)()
--- julia/simple_macro_example.jl
+++ julia/simple_macro_example.jl
@@ -1,11 +1,11 @@
 macro tenTimesX()
-  println("hello");     # executed at compile time
-  :(x * 10)             # expression returned at compile time
+  println("hello");     # executed at parse time
+  :(x * 10)             # expression returned at parse time
 end
 
 function f()
   x = 2;
-  @tenTimesX()          # replaced with x*10 at compile time
+  @tenTimesX()          # replaced with x*10 at parse time
 end                     # when called, f executes x=2; x*10
 
 @assert f() == 20
