ffe0e0e51f1e6b7259ec68c536b3cae3cb7fc72a
Ray Toal <rtoal@lmu.edu>
2016-01-02 14:23:13 -0800

    Two-space indentation for Erlang

--- erlang/non_streaming_triple.erl
+++ erlang/non_streaming_triple.erl
@@ -1,7 +1,7 @@
 % First line is ignored when running with escript
 main(_) ->
-    [io:format("~p, ~p, ~p~n", [A,B,C]) ||
-        C <- lists:seq(1,50),
-        B <- lists:seq(1,C),
-        A <- lists:seq(1,B),
-        A*A + B*B == C*C].
+  [io:format("~p, ~p, ~p~n", [A,B,C]) ||
+    C <- lists:seq(1,50),
+    B <- lists:seq(1,C),
+    A <- lists:seq(1,B),
+    A*A + B*B == C*C].
