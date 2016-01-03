644eb61fc949f08b53fdb2858103a31745717980
Ray Toal <rtoal@lmu.edu>
2016-01-02 23:50:34 -0800

    Better names

--- erlang/anagram.erl
+++ erlang/anagram.erl
@@ -1,8 +1,8 @@
 % First line is ignored when running with escript
 main([S]) ->
-  lists:foreach(fun(X) -> io:format("~s~n", [X]) end, permute(S));
+  lists:foreach(fun(X) -> io:format("~s~n", [X]) end, permutations(S));
 main(_) ->
   io:format("Exactly one argument is required\n").
 
-permute([]) -> [""];
-permute(S) -> [[H|T] || H<-S, T<-permute(S--[H])].
+permutations([]) -> [""];
+permutations(S) -> [[H|T] || H<-S, T<-permutations(S--[H])].
