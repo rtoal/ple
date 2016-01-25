7fff6f58761806681110d7cb7da11f4221e9efd1
Ray Toal <rtoal@lmu.edu>
2016-01-24 19:22:29 -0800

    Use JS string interpolation now

--- js/wordcount_ascii.js
+++ js/wordcount_ascii.js
@@ -4,10 +4,10 @@ let counts = new Map();
 
 reader.on('line', line => {
   for (let word of line.toLowerCase().match(/[a-z']+/g) || []) {
-    counts.set(word, (counts.get(word) || 0) + 1)
+    counts.set(word, (counts.get(word) || 0) + 1);
   }
 }).on('close', () => {
   for (let word of Array.from(counts.keys()).sort()) {
-    console.log('%s %d', word, counts.get(word))
+    console.log(`${word}, ${counts.get(word)}`);
   }
 });
