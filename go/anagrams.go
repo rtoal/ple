9b7c8bd67988ac4f6638c54870ea0a8651feadec
Ray Toal <rtoal@lmu.edu>
2016-01-30 13:41:22 -0800

    Implement Heap's Algorithm correctly

--- go/anagrams.go
+++ go/anagrams.go
@@ -19,13 +19,14 @@ func generatePermutations(a []string, n int) {
 	if n == 0 {
 		fmt.Println(strings.Join(a, ""))
 	} else {
-		for i := 0; i <= n; i++ {
+		for i := 0; i < n; i++ {
 			generatePermutations(a, n-1)
 			if n % 2 == 0 {
-				a[i], a[n] = a[n], a[i]
-			} else {
 				a[0], a[n] = a[n], a[0]
+			} else {
+				a[i], a[n] = a[n], a[i]
 			}
 		}
+		generatePermutations(a, n-1)
 	}
 }
