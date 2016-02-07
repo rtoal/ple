b72672c70d7136a8c98be792570550ba3c06be7f
Ray Toal <rtoal@lmu.edu>
2016-02-06 19:11:17 -0800

    Getting better at Elm

--- elm/triple.elm
+++ elm/triple.elm
@@ -1,11 +1,10 @@
-import Html exposing (ul, li, text)
-import List exposing (map, concatMap, filter, repeat)
-
-values =
-  [1..40] |> concatMap (\c ->
-    [1..c-1] |> concatMap (\b ->
-      [1..b-1] |> map (\a -> (a,b,c)))) |>
-        filter (\(a,b,c) -> a*a + b*b == c*c)
+import Graphics.Element exposing (show, flow, down)
+import List exposing (map, concatMap, filterMap)
 
 main =
-  values |> map (toString >> text >> repeat 1 >> li []) |> ul []
+  [1..40] |> concatMap (\c ->
+    [1..c-1] |> concatMap (\b ->
+      [1..b-1] |> filterMap (\a ->
+        (if a*a+b*b==c*c then Just (a,b,c) else Nothing))))
+    |> map show
+    |> flow down
