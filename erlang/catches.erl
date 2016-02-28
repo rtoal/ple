aa46f4a53fdee862c57c8c2e7d40b2e0c411baa2
Ray Toal <rtoal@lmu.edu>
2016-02-27 22:41:39 -0800

    Erlang catch examples

---
+++ erlang/catches.erl
@@ -0,0 +1,8 @@
+% First line is ignored when running with escript
+main(_) ->
+  10 = (catch 7 + 3),
+  {'EXIT', {badarith, _}} = (catch 10 * dog),
+  {'EXIT', {badarg, _}} = (catch dog ! 100),
+  {'EXIT', {if_clause, _}} = (catch if false -> ok end),
+  {'EXIT', 205} = (catch exit(205)),
+  205 = (catch throw(205)).
