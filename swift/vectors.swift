ca58d9d153b34c8933536280f73749ab4a009c76
Ray Toal <rtoal@lmu.edu>
2016-03-04 19:34:52 -0800

    Use sqrt, since 1/2 is actually 0 in isolation

--- swift/vectors.swift
+++ swift/vectors.swift
@@ -1,19 +1,19 @@
 import Foundation
 
 struct Vector: CustomStringConvertible {
-    
+
     var x: Double
     var y: Double
-    
+
     init(x: Double, y: Double){
         self.x = x;
         self.y = y;
     }
-    
+
     func magnititude() -> Double {
-        return pow(x * x + y * y, 1/2)
+        return sqrt(x * x + y * y)
     }
-    
+
     var description: String {
         return "<\(x),\(y)>"
     }
@@ -33,6 +33,5 @@ let v = Vector(x: -5, y: 10)
 assert(u.x == 3)
 assert(u.y == 4)
 assert(u.magnititude() == 5)
-print(String(u + v))
 assert(String(u + v) == "<-2.0,14.0>")
 assert(u * v == 25)
