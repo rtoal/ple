245f84812d4f0b3466d339ae8838a4671bffc78e
Matthew <mwflickner@gmail.com>
2016-03-04 21:00:22 -0500

    added vector class that matches python, erlang, lua implementations using doubles

---
+++ swift/vectors.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+struct Vector: CustomStringConvertible {
+    
+    var x: Double
+    var y: Double
+    
+    init(x: Double, y: Double){
+        self.x = x;
+        self.y = y;
+    }
+    
+    func magnititude() -> Double {
+        return pow(x*x + y*y, 1/2)
+    }
+    
+    var description: String {
+        return "<\(x),\(y)>"
+    }
+}
+
+func + (left: Vector, right: Vector) -> Vector {
+    return Vector(x: left.x + right.x, y: left.y + right.y)
+}
+
+func * (left: Vector, right: Vector) -> Double {
+    return left.x * right.x + left.y * right.y
+}
+
+let u = Vector(x: 3,y: 4)
+let v = Vector(x: -5,y: 10)
+
+assert(u.x == 3)
+assert(u.y == 4)
+assert(u.magnititude() == 5)
+print(String(u+v))
+assert(String(u+v) == "<-2.0,14.0>")
+assert(u * v == 25)
