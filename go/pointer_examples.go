1f90d20e99db9d8b019e0d7b886bb65ce13672c6
Ray Toal <rtoal@lmu.edu>
2016-03-07 07:37:26 -0800

    Go pointers, a stub

---
+++ go/pointer_examples.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// TODO - work in progress, asserts needed
+func main() {
+    type Node struct {
+        value float64
+        next *Node
+    }
+    var n1 Node
+    n2 := new(Node)
+    fmt.Printf("%v %v\n", q.i, t.i)
+    type ints []int
+    var p *ints = new(ints)
+    fmt.Printf("%v\n", p)
+}
