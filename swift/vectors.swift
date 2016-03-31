9afddbcf5af37561c9340d46ea9b1ea4c470e88b
Ray Toal <rtoal@lmu.edu>
2016-03-31 09:19:38 -0700

    Add prefix operator

--- swift/vectors.swift
+++ swift/vectors.swift
@@ -22,6 +22,10 @@ func * (left: Vector, right: Vector) -> Double {
     return left.i * right.i + left.j * right.j
 }
 
+prefix func - (v: Vector) -> Vector {
+    return Vector(i: -v.i, j: -v.j)
+}
+
 let u = Vector(i: 3, j: 4)
 let v = Vector(i: -5, j: 10)
 assert(u.i == 3)
@@ -29,3 +33,4 @@ assert(u.j == 4)
 assert(u.magnititude() == 5)
 assert(String(u + v) == "<-2.0,14.0>")
 assert(u * v == 25)
+assert(String(-v) == "<5.0,-10.0>")
