d6fec275626d3cf5e3ca1546cf275dee4e8050d1
Ray Toal <rtoal@lmu.edu>
2016-04-03 03:09:55 -0700

    Optional demo

--- java/OptionalDemo.java
+++ java/OptionalDemo.java
@@ -3,12 +3,17 @@ public class OptionalDemo {
     public static void main(String[] args) {
         Person alice = new Person("Alice");
         Person bob = new Person("Bob", alice);
-        Person carol = new Person("Carol", bob);
 
-        bob.getBoss().ifPresent(System.out::println);
-        bob.getBoss().ifPresent(boss -> {assert boss == alice;});
-        alice.getBoss().ifPresent(boss -> {assert false;});
-        assert alice.getBoss().orElse(carol) == carol;
+        bob.getBoss().ifPresent(p -> {assert p == alice;});
+        alice.getBoss().ifPresent(p -> {assert false;});
+        assert alice.getBoss().orElse(bob) == bob;
+        assert bob.getBoss().orElse(bob) == alice;
+
+        Optional<Person> b = bob.getBoss();
+        assert b.filter(p -> p.getName().startsWith("A")) == b;
+        assert !b.filter(p -> p.getName().startsWith("B")).isPresent();
+        assert b.map(Person::getName).orElse("").equals("Alice");
+        assert alice.getBoss().map(Person::getName).orElse("").equals("");
     }
 }
 
