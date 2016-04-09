2e1f5aa0a3fa9c6fbd6f7d8f1278bfc03f539a32
Ray Toal <rtoal@lmu.edu>
2016-04-09 01:59:17 -0700

    Let customer actually eat 10 meals and not starve

--- java/Customer.java
+++ java/Customer.java
@@ -14,14 +14,16 @@ public class Customer extends Restaurant.Agent {
 
     @Override public void run() {
         try {
-            for (int i = 0; i < 10; i++) {
+            for (int mealsEaten = 0; mealsEaten < 10;) {
                 if (Order.place(this, "pancakes", 7000)) {
                     Order order = meal.take();
                     act("eating " + order, 10000);
+                    mealsEaten++;
                 } else {
                     act("can't place order, may return later", 5000);
                 }
             }
+            log("going home");
         } catch (InterruptedException e) {
             log("banished from restaurant");
         }
