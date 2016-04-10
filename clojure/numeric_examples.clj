f86df7b63ed620d237405f7f32cd4938a87f6fbd
Ray Toal <rtoal@lmu.edu>
2016-04-10 01:45:43 -0700

    Improve Clojure overflow example

--- clojure/numeric_examples.clj
+++
@@ -1,9 +0,0 @@
-(use 'clojure.test)
-(def x (long 1E15))
-(is (thrown? ArithmeticException (* x x)))
-(is (= (*' x x) (bigint 1E30)))
-
-(is (= (type (int 3)) Integer))
-(is (= (type (/ 3 9)) clojure.lang.Ratio))
-(is (= (type (/ 8 4)) Long))
-(is (= (type (/ 3.0 9)) Double))
