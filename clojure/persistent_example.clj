2c4b755c4f9e026581475b26902f8ba88ca83f52
Ray Toal <rtoal@lmu.edu>
2016-04-12 08:34:31 -0700

    Explain persistent Clojure example better

--- clojure/persistent_example.clj
+++ clojure/persistent_example.clj
@@ -1,5 +1,6 @@
-(def a (list (repeat 100 :dog)))
-(def b (conj a :cat))
+(def a (list (repeat 1000 :dog)))   ; 1000-dog list
+(def b (conj a :cat))               ; new list, cat at front
 
 (use 'clojure.test)
 (is (identical? a (rest b)))
+(is (= (type a) clojure.lang.PersistentList))
