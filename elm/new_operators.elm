b8f132480da397ea7be09726fa68d38366e1563a
Ray Toal <rtoal@lmu.edu>
2016-04-23 17:32:01 -0700

    Use application operator in tests

--- elm/new_operators.elm
+++ elm/new_operators.elm
@@ -15,6 +15,6 @@ infix 2 >*<
 main : Element
 main =
   elementRunner <| suite "Test own operators with precedence"
-    [ test "high" ((8 * 3 <-*-> 5) `assertEqual` 88)  -- 8*(2*3+5)
-    , test "low" ((8 * 3 >*< 5) `assertEqual` 53)     -- 2*(8*2)+5
+    [ test "high" <| (8 * 3 <-*-> 5) `assertEqual` 88  -- 8*(2*3+5)
+    , test "low" <| (8 * 3 >*< 5) `assertEqual` 53     -- 2*(8*2)+5
     ]
