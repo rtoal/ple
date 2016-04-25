81a58dfb7cdfeb0ffa556779d06987c4be4871a0
Ray Toal <rtoal@lmu.edu>
2016-04-24 18:42:13 -0700

    Include type alias example for Elm

---
+++ elm/alias.elm
@@ -0,0 +1,12 @@
+type alias Person = { name : String, id : Int}
+type alias Widget = { name : String, id : Int}
+
+type PrimaryColor = Red | Blue | Green
+type alias PixelColor = PrimaryColor
+
+p : Person
+p = { name = "Alice", id = 239 }
+w : Widget
+w = p              -- Disappointing perhaps, but legal
+g : PixelColor
+g = Green
