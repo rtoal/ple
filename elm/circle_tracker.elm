8eb1323a4cb859cb2934cdce1f19a32321ceb4f0
Ray Toal <rtoal@lmu.edu>
2016-04-29 02:19:51 -0700

    New Elm example

--- elm/circle_tracker.elm
+++ elm/circle_tracker.elm
@@ -9,11 +9,11 @@ main =
   Signal.map3 scene Mouse.position Window.dimensions Mouse.isDown
 
 scene : (Int,Int) -> (Int,Int) -> Bool -> Element
-scene (x,y) (w,h) down =
+scene (mx,my) (w,h) down =
   let
-    (dx,dy) = (toFloat x - toFloat w/2, toFloat h/2 - toFloat y)
+    (x,y) = (toFloat mx - toFloat w/2, toFloat h/2 - toFloat my)
   in
     collage w h <|
       [circle (if down then 25 else 20)
         |> filled green
-        |> move (dx, dy)]
+        |> move (x, y)]
