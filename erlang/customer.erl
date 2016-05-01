18933330f2fb187998bb9f117fc928e6be9cfcfe
Ray Toal <rtoal@lmu.edu>
2016-05-01 01:02:45 -0700

    Improve Erlang concurrent examples

--- erlang/customer.erl
+++ erlang/customer.erl
@@ -4,18 +4,17 @@
 run(Name, Orders) ->
   eat(Name, Orders, 10).
 
+eat(Name, _, 0) ->
+  io:format("~s going home~n", [Name]);
 eat(Name, Orders, Times) ->
   Orders ! {place, {self(), "soup"}},
   receive
-    {serve, Meal} when Times > 0 ->
+    {serve, Meal} ->
       io:format("~s eating ~p~n", [Name, Meal]),
       timer:sleep(rand:uniform(5000)),
-      eat(Name, Orders, Times - 1);
-    _ when Times == 0 ->
-      ok
-    after 7000 ->
-      io:format("~s waiting too long, going shopping~n", [Name]),
-      timer:sleep(rand:uniform(5000)),
-      eat(Name, Orders, Times)
-  end,
-  io:format("~s going home~n", [Name]).
+      eat(Name, Orders, Times - 1)
+  after 7000 ->
+    io:format("~s waiting too long, going shopping~n", [Name]),
+    timer:sleep(rand:uniform(5000)),
+    eat(Name, Orders, Times)
+  end.
--- erlang/restaurant.erl
+++ erlang/restaurant.erl
@@ -4,4 +4,4 @@ main(_) ->
   [spawn(cook, run, [Name, Queue]) || Name <- ["Aly","Rob","Chi"]],
   [spawn(customer, run, [Name, Queue]) || Name <- ["Xia","Ann","Bo",
     "Rai","Ali","Kat","Eshe","Sen","Ami","Lina","Ara","Zuri"]],
-  timer:sleep(50000).
+  timer:sleep(180000).
