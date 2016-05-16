a7271ab4f90a75be3884ea59fe106dd844d3c879
Ray Toal <rtoal@lmu.edu>
2016-05-16 00:50:38 -0700

    Test cleanup

--- julia/prereq.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-which julia &> /dev/null
-if [[ $? -ne 0 ]]; then
-    echo Install Julia from http://julialang.org/ >&2
-    exit 1
-else
-    echo anagrams,triple,wordcount
-    exit 0
-fi
