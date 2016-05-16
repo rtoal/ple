010aac67ff9e83ce22c68a610be23d13026f040c
Ray Toal <rtoal@lmu.edu>
2016-05-16 14:36:49 -0700

    Revamp Lua tester

--- lua/prereq.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-which lua &> /dev/null
-if [[ $? -ne 0 ]]; then
-    echo Install Lua from http://lua.org/ >&2
-    exit 1
-else
-    echo anagrams,triple,wordcount
-    exit 0
-fi
