03045ebf02c58c51bfe71bd5e7a0890e2ff06c41
Ray Toal <rtoal@lmu.edu>
2016-05-15 23:48:59 -0700

    Python tests!

--- python/prereq.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-which python3 &> /dev/null
-if [[ $? -ne 0 ]]; then
-    echo Install Python 3 from https://www.python.org/ >&2
-    exit 1
-else
-    echo anagrams,triple,wordcount,animals
-    exit 0
-fi
