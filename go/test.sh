a62ec1598c27f94550241d81d0d47ef5861e091b
Ray Toal <rtoal@lmu.edu>
2016-05-16 12:37:34 -0700

    Add new files to the test in Go

--- go/test.sh
+++ go/test.sh
@@ -2,6 +2,7 @@
 go run anagrams.go rats | diff ../test/rats_heap_expected - && \
 go test animals_test.go && \
 go test arrays_test.go && \
+go test base64_error_test.go && \
 go test binarytree/binary_tree_test.go binarytree/binary_tree.go  && \
 go test channel_sum_test.go && \
 go test closed_channel_read_test.go && \
@@ -9,6 +10,7 @@ go test compound_statements_test.go && \
 go test copy_test.go && \
 go run cpu.go && \
 go test escape_test.go && \
+go test format_specifier_test.go && \
 go test functions_test.go && \
 go test hello_world_test.go && \
 go run interfaces.go && \
@@ -19,8 +21,10 @@ go test nil_maps_test.go && \
 go test pointer_test.go && \
 go run primes.go && \
 go test recover_test.go && \
+go test reflection_types_test.go && \
 go build restaurant/*.go && \
 go test shapes_test.go && \
+go test simple_json_test.go && \
 go test simple_types_test.go && \
 go test slice_test.go && \
 go test structs_test.go
