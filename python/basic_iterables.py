eb06fd78076fd76d37348620eeaf4d168288a71d
Ray Toal <rtoal@lmu.edu>
2016-05-16 00:53:14 -0700

    Found an old script using Python 2

--- python/basic_iterables.py
+++ python/basic_iterables.py
@@ -4,8 +4,8 @@ for x in {1,2,3}: print(x)       # elements of a set
 for c in 'hello': print(c)       # characters of a string
 
 for k in {'x':1, 'y':2, 'z':3}:
-    print k                      # keys of a dict
+    print(k)                     # keys of a dict
 
 with open('colors') as f:
     for line in f:               # lines of a file
-        print line.strip()
+        print(line.strip())
