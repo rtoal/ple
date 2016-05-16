1d170030e18874bf4c12b1c5657ed463d84891df
Ray Toal <rtoal@lmu.edu>
2016-05-15 23:27:18 -0700

    CoffeeScript tests

--- coffeescript/prereq.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-which coffee &> /dev/null
-if [[ $? -ne 0 ]]; then
-    echo Install CoffeeScript from https://www.coffeescript.org/ >&2
-    exit 1
-else
-    echo anagrams,animals,average_with_splats,chained_existential,circle
-    exit 0
-fi
