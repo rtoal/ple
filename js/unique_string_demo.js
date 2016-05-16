6d07650a70f4a43310ff291387aae4510018b29d
Ray Toal <rtoal@lmu.edu>
2016-05-15 22:42:01 -0700

    Old files no longer needed

--- js/unique_string_demo.js
+++
@@ -1,8 +0,0 @@
-import generateString, {countOf} from './string_generator.js';
-import assert from 'assert';
-
-assert(generateString('v') === 'v1');
-assert(generateString('v') === 'v2');
-assert(generateString('f') === 'f1');
-assert(generateString('v') === 'v3');
-assert(countOf('v') === 3)
