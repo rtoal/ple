ae8c0bb354dd953aa79c2fcdec12ccbfb09924e2
Ray Toal <rtoal@lmu.edu>
2016-05-15 22:44:37 -0700

    Someone forgot a comma

--- js/triple.js
+++ js/triple.js
@@ -2,7 +2,7 @@ for (let c = 1; c <= 40; c++) {
   for (let b = 1; b < c; b++) {
     for (let a = 1; a < b; a++) {
       if (a * a + b * b === c * c) {
-        console.log(`${a}, ${b} ${c}`);
+        console.log(`${a}, ${b}, ${c}`);
       }
     }
   }
