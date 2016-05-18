99b83add5f6126deccf15bc277b61e11be650f20
Ray Toal <rtoal@lmu.edu>
2016-05-17 20:06:55 -0700

    Descripton might be confused with existing protocols

--- swift/simple_protocol.swift
+++ swift/simple_protocol.swift
@@ -1,19 +1,19 @@
 import Foundation
 
-protocol Describable {
-    var description: String { get }
+protocol Summarizable {
+    var summary: String { get }
 }
 
-struct Circle: Describable {
+struct Circle: Summarizable {
     var radius = 1.0
-    var description: String {return "Circle with radius \(radius)"}
+    var summary: String {return "Circle with radius \(radius)"}
 }
 
-enum Direction: Int, Describable {
+enum Direction: Int, Summarizable {
     case North, East, South, West
-    var description: String {return "Bearing \(90 * self.rawValue)"}
+    var summary: String {return "Bearing \(90 * self.rawValue)"}
 }
 
-let a: [Describable] = [Circle(radius: 5), Direction.West]
-assert(a[0].description == "Circle with radius 5.0")
-assert(a[1].description == "Bearing 270")
+let a: [Summarizable] = [Circle(radius: 5), Direction.West]
+assert(a[0].summary == "Circle with radius 5.0")
+assert(a[1].summary == "Bearing 270")
