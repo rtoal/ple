c964b2b285f0a346abc4d3ef53f62757b705a066
Ray Toal <r.toal@criteo.com>
2016-05-20 10:20:23 -0700

    Fix up C anagrams

--- c/anagrams.c
+++ c/anagrams.c
@@ -2,34 +2,36 @@
 #include <stdio.h>
 #include <string.h>
 
-char* swap (char* source, int i, int j) {
+void swap(char *source, int i, int j) {
     char saved = source[i];
     source[i] = source[j];
     source[j] = saved;
 }
 
-void generate_permutations (int n, char* a) {
+void generate_permutations(char* a, int n) {
     if (n == 0) {
         printf("%s\n", a);
     } else {
-        for (int i = 0; i <= n; i++) {
-            generate_permutations(n - 1, a);
-            swap(a, n % 2 == 0 ? i : 0, n);
+        for (int i = 0; i < n; i++) {
+            generate_permutations(a, n-1);
+            swap(a, n % 2 == 0 ? 0 : i, n);
         }
+        generate_permutations(a, n-1);
     }
 }
 
-int main (int argc, const char* argv[]) {
+int main(int argc, const char* argv[]) {
+
     if (argc != 2) {
         fprintf(stderr, "Need exactly one argument!\n");
         return 1;
     }
 
     size_t len = strlen(argv[1]);
-    char * word = malloc(len + 1); // Remember null terminator
+    char *word = malloc(len + 1);
     word = strncpy(word, argv[1], len);
 
-    generate_permutations(len - 1, word);
+    generate_permutations(word, len-1);
 
     free(word);
     return 0;
