0a422f6007fab2b8c41cb0c0b6d76e941b3fb913
Ray Toal <r.toal@criteo.com>
2016-05-20 10:20:48 -0700

    Add asserts to C sum of even squares

--- c/sum_of_even_squares.c
+++ c/sum_of_even_squares.c
@@ -7,3 +7,20 @@ int sum_of_even_squares(int* a, unsigned int length) {
     }
     return total;
 }
+
+#include <assert.h>
+
+static int a[] = {7, 3, -8, 4, 1, 0, 11, 2};
+
+int main() {
+    assert(sum_of_even_squares(a, 0) == 0);
+    assert(sum_of_even_squares(a, 1) == 0);
+    assert(sum_of_even_squares(a, 2) == 0);
+    assert(sum_of_even_squares(a, 3) == 64);
+    assert(sum_of_even_squares(a, 4) == 80);
+    assert(sum_of_even_squares(a, 5) == 80);
+    assert(sum_of_even_squares(a, 6) == 80);
+    assert(sum_of_even_squares(a, 7) == 80);
+    assert(sum_of_even_squares(a, 8) == 84);
+    return 0;
+}
