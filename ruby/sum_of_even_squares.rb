d8e7f921dbed8c81d98ac577ac18b2e14a03ba9c
Ray Toal <r.toal@criteo.com>
2016-05-19 23:32:15 -0700

    Add tests to Ruby squares

--- ruby/sum_of_even_squares.rb
+++ ruby/sum_of_even_squares.rb
@@ -1,3 +1,9 @@
 def sum_of_even_squares(a)
   a.select{|x| x % 2 == 0}.map{|x| x * x}.reduce(0, :+)
 end
+
+fail unless sum_of_even_squares([]) == 0
+fail unless sum_of_even_squares([1]) == 0
+fail unless sum_of_even_squares([1,2]) == 4
+fail unless sum_of_even_squares([1,2,3]) == 4
+fail unless sum_of_even_squares([1,2,3,4]) == 20
