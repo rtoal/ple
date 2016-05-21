8d67872a31c507bbb9fdecd5059c2544b5583150
Ray Toal <rtoal@lmu.edu>
2016-05-21 14:43:01 -0700

    Update animals example

--- cpp/animals.cpp
+++ cpp/animals.cpp
@@ -1,50 +1,52 @@
 #include <string>
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 class Animal {
 protected:
-    string name;
-    virtual string sound() = 0;
+  string name;
+  virtual string sound() = 0;
 public:
-    Animal(string name): name(name) {}
-    void speak() {
-        cout << name << " says " << sound() << endl;
-    }
+  Animal(string name): name(name) {}
+  string speak() {
+    return (std::stringstream() << name << " says " << sound()).str();
+  }
 };
 
 class Cow: public Animal {
 protected:
-    string sound() {
-        return "moooo";
-    }
+  string sound() {
+    return "moooo";
+  }
 public:
-    Cow(string name): Animal(name) {}
+  Cow(string name): Animal(name) {}
 };
 
 class Horse: public Animal {
 protected:
-    string sound() {
-        return "neigh";
-    }
+  string sound() {
+    return "neigh";
+  }
 public:
-    Horse(string name): Animal(name) {}
+  Horse(string name): Animal(name) {}
 };
 
 class Sheep: public Animal {
 protected:
-    string sound() {
-        return "baaaa";
-    }
+  string sound() {
+    return "baaaa";
+  }
 public:
-    Sheep(string name): Animal(name) {}
+  Sheep(string name): Animal(name) {}
 };
 
+#include <cassert>
 int main(int, char**) {
-    Animal* h = new Horse("CJ");
-    h->speak();
-    Animal* c = new Cow("Bessie");
-    c->speak();
-    (new Sheep("Little Lamb"))->speak();
-    return 0;
+  Animal* h = new Horse("CJ");
+  assert(h->speak() == "CJ says neigh");
+  Animal* c = new Cow("Bessie");
+  assert(c->speak() == "Bessie says moooo");
+  assert((new Sheep("Little Lamb"))->speak() == "Little Lamb says baaaa");
+  return 0;
 }
