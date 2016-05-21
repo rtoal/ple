05a0a2915a584146db67905fd16350f480e03bbc
Ray Toal <rtoal@lmu.edu>
2016-05-21 13:18:20 -0700

    Clojure tests

---
+++ clojure/test.sh
@@ -0,0 +1,40 @@
+#/usr/bin/env bash
+clj agent_example.clj && \
+clj animals_multi.clj rats && \
+clj animals_protocol.clj && \
+clj arithmetic.clj && \
+clj average.clj && \
+clj basic_types.clj && \
+clj count_and_conj.clj && \
+clj division.clj && \
+clj energy.clj && \
+clj fib.clj && \
+clj improper_var_use.clj && \
+clj infix.clj && \
+clj more_collection_examples.clj && \
+clj namespace_example.clj && \
+clj or.clj && \
+clj overflow.clj && \
+clj persistent_example.clj && \
+clj shape_multi.clj && \
+clj shape_protocol.clj && \
+clj simplest_macro.clj && \
+clj stm.clj && \
+clj sum_of_even_squares.clj && \
+clj thread_locking.clj && \
+clj thread_mess.clj && \
+clj triple.clj | diff ../test/triple_expected - && \
+clj unless.clj && \
+clj varargs.clj && \
+clj vars.clj && \
+clj word_count.clj < ../test/wordcount_ascii_input | diff ../test/wordcount_ascii_expected - && \
+clj word_count_slurp.clj < ../test/wordcount_ascii_input | diff ../test/wordcount_ascii_expected -
+
+if [ $? -ne 0 ]; then
+    echo
+    echo "***CLOJURE TESTS FAILED ***"
+    exit 1
+else
+    echo
+    echo "CLOJURE TESTS PASSED"
+fi
