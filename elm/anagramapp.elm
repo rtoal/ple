4a26c6993865e0a21f211a352ebdd701cfd547c2
Ray Toal <rtoal@lmu.edu>
2016-05-27 01:08:38 -0700

    Nicer Message type

--- elm/anagramapp.elm
+++ elm/anagramapp.elm
@@ -6,13 +6,13 @@ import Html.Events exposing (onInput, targetValue)
 import Html.App as Html
 
 type alias Model = String
-type alias Msg = String
+type Message = ChangeTo String
 
 main =
   Html.beginnerProgram { model = "", view = view, update = update }
 
-update message model =
-  message
+update (ChangeTo newModel) model =
+  newModel
 
 view model =
   div []
@@ -20,6 +20,6 @@ view model =
       [ placeholder "Text to anagram"
       , value model
       , maxlength 6
-      , onInput (\s -> s)
+      , onInput ChangeTo
       ]
       []) :: map (\s -> div [] [text s]) (anagrams model))
