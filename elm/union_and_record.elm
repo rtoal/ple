bb7c5e974c1002da82dbe84c2ed8dfd757bb2ce3
Ray Toal <rtoal@lmu.edu>
2016-05-27 00:14:00 -0700

    Changes for Elm 0.17

--- elm/union_and_record.elm
+++ elm/union_and_record.elm
@@ -1,9 +1,10 @@
 type Person
-  = Person { name : String, id: Int }
+  = Person { name : String, id : Int }
 type Widget
-  = Widget { name : String, id: Int }
+  = Widget { name : String, id : Int }
 
 p : Person
-p = Person { name = "Alice", id: 239 }
-w : Widget
--- w = p is now not allowed
+p = Person { name = "Alice", id = 239 }
+-- We now cannot say
+-- w : Widget
+-- w = p
