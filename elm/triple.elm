8ff99d66ea7eb1d7869509dc1b01333d623601b4
Ray Toal <rtoal@lmu.edu>
2016-05-27 00:10:14 -0700

    Favor >> over <<

--- elm/triple.elm
+++ elm/triple.elm
@@ -6,5 +6,5 @@ main =
     [1..c-1] |> concatMap (\b ->
       [1..b-1] |> filterMap (\a ->
         (if a*a+b*b==c*c then Just (a,b,c) else Nothing))))
-    |> map (div [] << repeat 1 << text << toString)
+    |> map (toString >> text >> repeat 1 >> div [])
     |> div []
