9fef80b1c71e6959842bb596b625bd76a726d518
Ray Toal <rtoal@lmu.edu>
2016-05-27 23:54:20 -0700

    Upgrade mouse tracker to 0.17

--- elm/mouse_tracker.elm
+++ elm/mouse_tracker.elm
@@ -1,6 +1,32 @@
-import Graphics.Element exposing (Element, show)
-import Mouse
+import Html exposing (Html, text, div)
+import Html.App as App
+import Mouse exposing (moves)
 
-main : Signal Element
 main =
-  Signal.map show Mouse.position
+  App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+type alias Model = (Int, Int)
+
+init : (Model, Cmd Msg)
+init =
+  ((0,0), Cmd.none)
+
+type Msg
+  = Position Int Int
+
+update: Msg -> Model -> (Model, Cmd Msg)
+update (Position x y) model =
+  ((x, y), Cmd.none)
+
+subscriptions: Model -> Sub Msg
+subscriptions model =
+  Mouse.moves (\{x, y} -> Position x y)
+
+view: Model -> Html Msg
+view model =
+  text (toString model)
