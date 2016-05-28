ec4feeacde11f6a951622da95caec92452a055e3
Ray Toal <rtoal@lmu.edu>
2016-05-28 13:46:16 -0700

    Make Rust tests look like the others

--- rust/test.sh
+++ rust/test.sh
@@ -1,5 +1,22 @@
 #/usr/bin/env bash
-rustc --out-dir $TEMPDIR/ $TESTDIR/$TESTNAME.rs &> /dev/null && $TEMPDIR/$TESTNAME $TESTARGS
-EXIT_CODE=$?
-rm -rf $TEMPDIR/$TESTNAME
-exit $EXIT_CODE
+
+# The Rust directory is highly experimental. It needs to be cleaned up badly.
+
+rustc anagrams.rs && ./anagrams rats | diff ../test/rats_heap_expected - && \
+rustc animals.rs && ./animals && \
+rustc binding.rs && ./binding && \
+rustc borrow_fixed.rs && ./borrow_fixed && \
+rustc circle.rs && ./circle && \
+rustc safe_string_sort.rs && ./safe_string_sort && \
+rustc static_or_dynamic_scope.rs && ./static_or_dynamic_scope && \
+rustc triple.rs | ./triple | diff ../test/triple_expected - && \
+rustc wordcount.rs && ./wordcount < ../test/wordcount_ascii_input | diff ../test/wordcount_ascii_expected -
+
+if [ $? -ne 0 ]; then
+    echo
+    echo "*** RUST TESTS FAILED ***"
+    exit 1
+else
+    echo
+    echo "RUST TESTS PASSED"
+fi
