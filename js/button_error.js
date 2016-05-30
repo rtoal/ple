d8189d574af65af47cc1ea4697351fa862a9e820
Ray Toal <rtoal@lmu.edu>
2016-05-29 23:18:57 -0700

    Avoid global variable, fixes Issue 28

--- js/button_error.js
+++ js/button_error.js
@@ -1,6 +1,6 @@
 // Rookie mistake: alerts 10 for every button.
 for (var i = 0; i < 10; i++) {
-  button = document.createElement("button");
+  let button = document.createElement("button");
   button.innerHTML = i;
   button.addEventListener('click', e => {alert(i);});
   document.body.appendChild(button);
