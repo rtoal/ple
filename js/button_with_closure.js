c1900c056126db1f89a0b772f9c43764444c6c17
Ray Toal <rtoal@lmu.edu>
2016-05-29 23:20:31 -0700

    Another fix for issue 28

--- js/button_with_closure.js
+++ js/button_with_closure.js
@@ -1,5 +1,5 @@
 for (var i = 0; i < 10; i++) {
-  button = document.createElement("button");
+  let button = document.createElement("button");
   button.innerHTML = i;
   button.onclick = (function (i) {
     return function () {alert(i);}
