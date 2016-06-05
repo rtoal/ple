3c5a166bb99f77bd6fc5a4d617583bbd6aa0d352
Ray Toal <rtoal@lmu.edu>
2016-06-04 23:25:45 -0700

    Fix type in comment

--- rust/src/bin/move.rs
+++ rust/src/bin/move.rs
@@ -2,6 +2,6 @@ fn main() {
     let a = vec![0, 1, 2];  // owned by variable a
     let b = a;              // now owned by variable b
 
-    // println!("{:?}", b); will not compile!
+    // println!("{:?}", a); will not compile!
     println!("{:?}", b);    // This works, b is the owner
 }                           // vector deallocated, b out of scope
