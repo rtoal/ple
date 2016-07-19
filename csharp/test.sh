27466204f800243751edbbc0948a7c69141de79c
Ray Toal <rtoal@lmu.edu>
2016-07-19 00:45:17 -0700

    Add word count script to test

--- csharp/test.sh
+++ csharp/test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 mcs Greeter.cs && mono Greeter.exe && \
-mcs Tripler.cs && mono Tripler.exe | diff ../test/triple_expected -
+mcs Tripler.cs && mono Tripler.exe | diff ../test/triple_expected - && \
+mcs TraditionalWordCounter.cs && mono TraditionalWordCounter.exe < ../test/wordcount_input | diff ../test/wordcount_expected -
+
 
 if [ $? -ne 0 ]; then
     echo
