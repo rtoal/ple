cdedc5f5178ab81d836bd0abaababfaff2a68b42
Ray Toal <rtoal@lmu.edu>
2016-07-19 11:04:15 -0700

    Fluent C# word counter

--- csharp/test.sh
+++ csharp/test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 mcs Greeter.cs && mono Greeter.exe && \
 mcs Tripler.cs && mono Tripler.exe | diff ../test/triple_expected - && \
-mcs TraditionalWordCounter.cs && mono TraditionalWordCounter.exe < ../test/wordcount_input | diff ../test/wordcount_expected -
+mcs TraditionalWordCounter.cs && mono TraditionalWordCounter.exe < ../test/wordcount_input | diff ../test/wordcount_expected - && \
+mcs TraditionalWordCounter.cs && mono TraditionalWordCounter.exe < ../test/wordcount_ascii_input | diff ../test/wordcount_ascii_expected - && \
+mcs WordCounter.cs && mono WordCounter.exe < ../test/wordcount_input | diff ../test/wordcount_expected - && \
+mcs WordCounter.cs && mono WordCounter.exe < ../test/wordcount_ascii_input | diff ../test/wordcount_ascii_expected -
 
 
 if [ $? -ne 0 ]; then
