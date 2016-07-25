68b2ec70aa96501a07af40998dcc03f854307d96
Ray Toal <rtoal@lmu.edu>
2016-07-25 12:29:57 -0700

    More Haskell

--- haskell/quicksort.hs
+++ haskell/quicksort.hs
@@ -1,9 +1,8 @@
-quickSort :: Ord a => [a] -> [a]
+quickSort :: (Ord a) => [a] -> [a]
 quickSort []     = []
 quickSort (x:xs) = quickSort [a | a <- xs, a < x]
                    ++ [x] ++
                    quickSort [a | a <- xs, a >= x]
 
-main = do
-  print([1,2,3,4,5])
-  putStrLn("There")
+main =
+  print $ (quickSort [5,3,4,1,2,6] == [1..6]) || error("fail")
---
+++ haskell/typedefs.hs
@@ -0,0 +1,20 @@
+-- Define new types
+data PrimaryColor = Red | Green | Blue
+data Shape = Circle Float | Rectangle Float Float
+data Response a = Ok a | Error String
+data Tree a = Leaf a | Internal a [Tree a]
+
+-- Just make an alias, no new types are defined
+type Point = (Float, Float)
+type Polygon = [Point]
+
+-- Define functions on cases
+size :: Tree a -> Int
+size (Leaf _) = 1
+size (Internal _ children) = 1 + foldr (+) 0 (map size children)
+
+main =
+  let
+    t = Internal 7 [Leaf 2, Internal 3 [Leaf 9, Leaf 8, Leaf 1]]
+  in
+    print (size t == 6 || error "fail")
