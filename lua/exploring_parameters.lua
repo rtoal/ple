e8da0178e73bdc352510088980d1fab0e33f31ff
Ray Toal <rtoal@lmu.edu>
2021-03-04 13:03:07 -0800

    Make an example simpler

--- lua/exploring_parameters.lua
+++ lua/exploring_parameters.lua
@@ -2,10 +2,10 @@ x = {1, 2, 3}
 y = {4, 5, 6}
 
 function f(a, b)
-  a = 300              -- changing the parameter
+  a = 10               -- changing the parameter
   assert(x[1] == 1)    -- ...does not affect the argument
-  b[2] = 100           -- changing a field
-  assert(y[2] == 100)  -- ...does affect the argument
+  b[2] = 20            -- changing a field
+  assert(y[2] == 20)   -- ...does affect the argument
 end
 
 f(x, y)
