f28fc3e9f042d8907b410279aca72bc6c223147c
Ray Toal <rtoal@lmu.edu>
2021-03-04 16:41:02 -0800

    Refresh JavaScript tests

--- js/test.sh
+++ js/test.sh
@@ -13,6 +13,7 @@ node generator.js && \
 node method_example.js && \
 node method_example_short_syntax.js && \
 node parameters.js && \
+node permutations.mjs one two three && \
 node primitives_and_references.js && \
 node promise_demo.js && \
 node reference_error.js && \
@@ -21,8 +22,9 @@ node shallow.js && \
 node simple_scopes.js && \
 node static_or_dynamic_scope.js && \
 node this_examples.js && \
-node triple.js | diff ../test/triple_expected - && \
-node wordcount.js < ../test/wordcount_input | diff ../test/wordcount_expected -
+node top_ten_scorers.mjs < ../test/players_input | diff ../test/players_expected - && \
+node triple.mjs | diff ../test/triple_expected - && \
+node wordcount.mjs < ../test/wordcount_input | diff ../test/wordcount_expected -
 
 if [ $? -ne 0 ]; then
     echo
