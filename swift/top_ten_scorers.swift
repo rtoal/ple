d54f687f04f7b63c94f46ae287e2befcea3e894a
Ray Toal <rtoal@lmu.edu>
2021-03-28 09:51:10 -0700

    Modernize Swift top_ten_scorers

--- swift/top_ten_scorers.swift
+++ swift/top_ten_scorers.swift
@@ -2,38 +2,33 @@ import Foundation
 
 struct MalformedLine: Error {}
 
-extension String {
-    func padRight(to count: Int) -> String {
-        return "\(self)\(String(repeating: " ", count: count - self.count))"
-    }
-}
-
 struct Player {
     let name: String
     let team: String
     let games: Int
     let points: Int
     var ppg: Double { return Double(points) / Double(games) }
-    init(_ line: [String]) throws {
-        self.name = line[1]
-        self.team = line[0]
-        self.games = try Int(line[2]) ?? { throw MalformedLine() }()
-        self.points = try Int(line[3]) ?? { throw MalformedLine() }()
-    }
 }
 
 var players = [Player]()
-
 do {
     while let line = readLine() {
-        let player = try Player(line.components(separatedBy: ","))
+        let info = line.components(separatedBy: ",")
+        let player = Player(
+          name: info[1],
+          team: info[0],
+          games: try Int(info[2]) ?? { throw MalformedLine() }(),
+          points: try Int(info[3]) ?? { throw MalformedLine() }()
+        )
         if player.games >= 15 {
             players.append(player)
         }
     }
     for player in players.sorted(by: {$0.ppg > $1.ppg})[..<10] {
-        let name = player.name.padRight(to: 22)
-        let team = player.team.padRight(to: 4)
+        let name = player.name.padding(
+            toLength: 22, withPad: " ", startingAt: 0)
+        let team = player.team.padding(
+            toLength: 4, withPad: " ", startingAt: 0)
         print("\(name)\(team)\(String(format: "%8.2f", player.ppg))")
     }
 } catch is MalformedLine {
