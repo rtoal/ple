7b5d23436c3776f8fceea8848712bc76107d5b87
Ray Toal <rtoal@lmu.edu>
2021-04-25 01:29:03 -0700

    Use where in Swift triple script

--- swift/triple.swift
+++ swift/triple.swift
@@ -1,9 +1,7 @@
 for c in 1...40 {
     for b in 1..<c {
-        for a in 1..<b {
-            if a * a + b * b == c * c {
-                print("\(a), \(b), \(c)")
-            }
+        for a in 1..<b where a * a + b * b == c * c {
+            print("\(a), \(b), \(c)")
         }
     }
 }
