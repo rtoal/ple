82e3fdc8d3dc9d9f15c010012d2a320895832a41
Ray Toal <rtoal@lmu.edu>
2021-05-04 01:03:03 -0700

    Shorten Go Maps example

--- go/compound_statements_test.go
+++ go/compound_statements_test.go
@@ -3,24 +3,20 @@ package main
 import "fmt"
 
 func Example() {
-
 	capitals := map[string]string{
-		"NSW": "Sydney",
-		"VIC": "Melbourne",
-		"TAS": "Hobart",
-		"WA": "Perth",
-		"QLD": "Brisbane",
-		"SA": "Adelaide",
+		"Netherlands":  "Amsterdam",
+		"Aruba":        "Oranjestad",
+		"Curaçao":      "Willemstad",
+		"Sint Maarten": "Philipsburg",
 	}
-
-	for _, state := range [2]string{"NSW", "AL"} {
-		if capital, known := capitals[state]; known {
-			fmt.Printf("The capital of %s is %s\n", state, capital)
+	for _, country := range [2]string{"Aruba", "Wales"} {
+		if capital, known := capitals[country]; known {
+			fmt.Printf("The capital of %s is %s\n", country, capital)
 		} else {
-			fmt.Printf("I don't know the capital of %s\n", state)
+			fmt.Printf("I don't know the capital of %s\n", country)
 		}
 	}
 	// Output:
-	// The capital of NSW is Sydney
-	// I don't know the capital of AL
+	// The capital of Aruba is Oranjestad
+	// I don't know the capital of Wales
 }
