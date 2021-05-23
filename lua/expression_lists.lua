b8ea780767b7e97ef0a09bf6b62b750c24e4dd1b
Ray Toal <rtoal@lmu.edu>
2021-05-23 12:40:06 -0700

    Fix Lua comment, we want nil, not undefined

--- lua/expression_lists.lua
+++ lua/expression_lists.lua
@@ -1,5 +1,5 @@
 a, b = 5, 6, 7          -- extra results ignored
-c, d, e, f = 5, 6, 7    -- extra vars get undefined
+c, d, e, f = 5, 6, 7    -- extra vars become nil
 
 assert(a == 5 and b == 6)
 assert(c == 5 and d == 6 and e == 7 and f == nil)
