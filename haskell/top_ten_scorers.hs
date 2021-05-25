0073514b5d94b4eaaaf0639e69c4e31930ec57dd
Strieker <sspink157@gmail.com>
2021-05-25 11:48:29 -0700

    first big attempt at completing top_ten_scorers.hs --warning contains errors

--- haskell/top_ten_scorers.hs
+++ haskell/top_ten_scorers.hs
@@ -1,9 +1,35 @@
 import System.IO (getContents)
 import Data.Text (pack, splitOn)
+import Data.List
 
-main = 
+data Player = Player { name :: String
+                 , team :: String
+                 , ppg :: Int
+                 } deriving (Show)
+
+
+getTopScores :: [[String]] -> [Player]
+getTopScores players topScoringPlayers = 
+    case (lookup player players) of 
+        (player !! 3)::Int players) >= 15 -> topScores : Player ((player !! 1), (player !! 0), (ceiling (((player !! 3)::RealFrac)/((player !! 2)::RealFrac))))
+        False -> return ()
+    return topScores
+
+
+sortPlayers :: [Player] -> [Player]
+sortPlayers player1 player2
+    | (ppg player1) < (ppg player2) = GT
+    | (ppg player1) > (ppg player2) = LT
+    | (ppg player1) == (ppg player2) = compare (ppg player1) (ppg player2)
+
+main = do
+  topScorers = []
   getContents >>= (mapM_ (putStrLn . show . splitOn (pack ",") . pack) . lines)
-
+  players <- getContents
+  getTopScores players topScorers
+  sortBy sortPlayers topScorers
+  take 10 topScorers
+    
 -- reader.on("line", line => {
 --   let [team, name, games, points] = line.split(",")
 --   if (games >= 15) {
@@ -18,3 +44,4 @@ main =
 --     )
 --   }
 -- })
+
