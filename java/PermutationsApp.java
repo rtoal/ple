3a5758f243c6258b829d331bec5914ea738d85e0
Ray Toal <rtoal@lmu.edu>
2021-05-28 22:38:05 -0700

    Java permutations program

---
+++ java/PermutationsApp.java
@@ -0,0 +1,23 @@
+public class PermutationsApp {
+    public static void main(String[] args) {
+        printPermutations(args, args.length-1);
+    }
+
+    private static void printPermutations(String[] a, int n) {
+        if (n <= 0) {
+            System.out.println(String.join("\t", a));
+        } else {
+            for (int i = 0; i < n; i++) {
+                printPermutations(a, n-1);
+                swap(a, n % 2 == 0 ? 0 : i, n);
+            }
+            printPermutations(a, n-1);
+        }
+    }
+
+    private static void swap(String[] a, int i, int j) {
+        var saved = a[i];
+        a[i] = a[j];
+        a[j] = saved;
+    }
+}
