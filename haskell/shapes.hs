5d4dcd5441cc5c494298e56d17da2911a25cebcc
Strieker <sspink157@gmail.com>
2021-06-26 14:44:05 -0700

    created shapes.hs file that combines shapes.elm and shapes_test.elm and converts them to haskell, since those files were being used instead of the haskell version in the algebraic data types section of the haskell chapter

---
+++ haskell/shapes.hs
@@ -0,0 +1,26 @@
+import Control.Monad (guard)
+
+data Shape
+    = Circle Float
+    | Rectangle Float Float
+
+area :: Shape -> Float
+area s =
+    case s of
+        Circle r -> pi * r * r
+        Rectangle h w -> h * w
+
+perimeter :: Shape -> Float
+perimeter s =
+    case s of
+        Circle r ->
+            2 * pi * r
+        Rectangle h w ->
+            2 * (h + w)
+
+main = 
+    do
+        guard $ area (Circle 10) == (100 * pi)
+        guard $ perimeter (Circle 10) == (20 * pi)
+        guard $ area (Rectangle 2 8) == 16
+        guard $ perimeter (Rectangle 2 8) == 20
