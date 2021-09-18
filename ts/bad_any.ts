b1b95566eb88167cd165dfce76fa5673f5c7b46e
Ray Toal <rtoal@lmu.edu>
2021-09-18 13:06:07 -0700

    TypeScript work

---
+++ ts/bad_any.ts
@@ -0,0 +1,8 @@
+import * as assert from "assert"
+
+function next(n: number): any {
+  return n + 1
+}
+
+assert.equal(next(1), 2)
+assert.throws(() => next(1)(2), /TypeError/)
---
+++ ts/test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# tsc --strict animals.ts && node animals.js && \
+tsc --strict bad_any.ts && node bad_any.js && \
+tsc --strict top_ten_scorers.ts && node top_ten_scorers.js < ../test/wnba_input | diff ../test/wnba_expected - && \
+tsc --strict triple.ts && node triple.js | diff ../test/triple_expected - && \
+
+if [ $? -ne 0 ]; then
+    echo
+    echo "*** TYPESCRIPT TESTS FAILED ***"
+    exit 1
+else
+    echo
+    echo "TYPESCRIPT TESTS PASSED"
+fi
