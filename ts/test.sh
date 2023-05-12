0b87f17dd4b7116518819912ed1cdf75859bf75a
Ray Toal <rtoal@lmu.edu>
2023-05-11 20:39:20 -0700

    Cleaner TypeScript tests

--- ts/test.sh
+++ ts/test.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-# tsc --strict animals.ts && node animals.js && \
-tsc --strict --target esnext --module esnext alias.ts && node alias.js && \
-tsc --strict --target esnext --module esnext object_types.ts && node object_types.js && \
-tsc --strict --target esnext --module esnext bad_any.ts && node bad_any.js && \
-tsc --strict --target esnext --module esnext top_ten_scorers.ts && node top_ten_scorers.js < ../test/wnba_input | diff ../test/wnba_expected - && \
-tsc --strict --target esnext --module esnext triple.ts && node triple.js | diff ../test/triple_expected - && \
+tsc && \
+cd dist && \
+node alias.js && \
+node object_types.js && \
+node bad_any.js && \
+node top_ten_scorers.js < ../../test/wnba_input | diff ../../test/wnba_expected - && \
+node triple.js | diff ../../test/triple_expected - && \
 
 if [ $? -ne 0 ]; then
     echo
     echo "*** TYPESCRIPT TESTS FAILED ***"
+    cd ..
     exit 1
 else
     echo
     echo "TYPESCRIPT TESTS PASSED"
+    cd ..
 fi
