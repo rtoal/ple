871328aa1009eeff57424711d4c2006e0e5f26c6
Ray Toal <rtoal@lmu.edu>
2023-07-04 10:19:50 -0700

    Improve swift top ten scorers

--- swift/top_ten_scorers.swift
+++ swift/top_ten_scorers.swift
@@ -1,7 +1,5 @@
 import Foundation
 
-struct MalformedLine: Error {}
-
 struct Player {
     let name: String
     let team: String
@@ -11,26 +9,24 @@ struct Player {
 }
 
 var players = [Player]()
-do {
-    while let line = readLine() {
-        let info = line.components(separatedBy: ",")
-        let player = Player(
-          name: info[1],
-          team: info[0],
-          games: try Int(info[2]) ?? { throw MalformedLine() }(),
-          points: try Int(info[3]) ?? { throw MalformedLine() }()
-        )
-        if player.games >= 15 {
-            players.append(player)
-        }
+while let line = readLine() {
+    let info = line.components(separatedBy: ",")
+    guard let games = Int(info[2]) else { continue }
+    guard let points = Int(info[3]) else { continue }
+    let player = Player(
+        name: info[1],
+        team: info[0],
+        games: games,
+        points: points
+    )
+    if player.games >= 15 {
+        players.append(player)
     }
-    for player in players.sorted(by: {$0.ppg > $1.ppg})[..<10] {
-        let name = player.name.padding(
-            toLength: 22, withPad: " ", startingAt: 0)
-        let team = player.team.padding(
-            toLength: 4, withPad: " ", startingAt: 0)
-        print("\(name)\(team)\(String(format: "%8.2f", player.ppg))")
-    }
-} catch is MalformedLine {
-    print("Error reading file: line is malformed")
+}
+for player in players.sorted(by: {$0.ppg > $1.ppg})[..<10] {
+    let name = player.name.padding(
+        toLength: 22, withPad: " ", startingAt: 0)
+    let team = player.team.padding(
+        toLength: 4, withPad: " ", startingAt: 0)
+    print("\(name)\(team)\(String(format: "%8.2f", player.ppg))")
 }
