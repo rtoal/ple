209b96855604690daf858e809ebab377c707ec4f
Ray Toal <rtoal@lmu.edu>
2023-07-16 18:05:19 -0700

    Better Rust style

--- rust/src/bin/triple.rs
+++ rust/src/bin/triple.rs
@@ -1,9 +1,9 @@
 fn main() {
-    for c in 1 .. 41 {
-        for b in 1 .. c {
-            for a in 1 .. b {
+    for c in 1..=40 {
+        for b in 1..c {
+            for a in 1..b {
                 if a * a + b * b == c * c {
-                    println!("{}, {}, {}", a, b, c)
+                    println!("{}, {}, {}", a, b, c);
                 }
             }
         }
