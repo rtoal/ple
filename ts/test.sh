f5bd66ed6191f16add499ae0667a5fa74b81726e
Ray Toal <rtoal@lmu.edu>
2023-08-28 15:43:37 -0700

    TypeScript tests

--- ts/test.sh
+++ ts/test.sh
@@ -2,10 +2,26 @@
 tsc && \
 cd dist && \
 node alias.js && \
-node object_types.js && \
+node animals.js && \
+node assertion_functions.js && \
 node bad_any.js && \
+node bracket.js && \
+node classes.js && \
+node compose.js && \
+node destructuring.js && \
+node index_signatures.js && \
+node narrow.js && \
+node object_types.js && \
+node optionals.js && \
+node permutations.js I like carrots | diff ../../test/carrots_expected - && \
+node queue.js && \
+node satisfies.js && \
+node shapes.js && \
 node top_ten_scorers.js < ../../test/wnba_input | diff ../../test/wnba_expected - && \
 node triple.js | diff ../../test/triple_expected - && \
+node type_assertions.js && \
+node type_predicates.js && \
+node utility_types.js && \
 
 if [ $? -ne 0 ]; then
     echo
