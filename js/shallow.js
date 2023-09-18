b3567c308d082a4eddfd0cc6d0a8f8465b9a4cfc
Ray Toal <rtoal@lmu.edu>
2023-09-18 16:12:44 -0700

    Apply prettier

--- js/shallow.js
+++ js/shallow.js
@@ -1,4 +1,8 @@
-const a = [{x:0, y:0}, {x:3, y:0}, {x: 3, y:4}];
+const a = [
+  { x: 0, y: 0 },
+  { x: 3, y: 0 },
+  { x: 3, y: 4 },
+]
 
-const b = a;          // copies the reference, nothing more
-const c = a.slice();  // makes a SHALLOW COPY of array elements
+const b = a // copies the reference, nothing more
+const c = a.slice() // makes a SHALLOW COPY of array elements
