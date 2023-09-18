a9ee28328e9c4421b8ecd584104297f436477449
Ray Toal <rtoal@lmu.edu>
2023-09-18 01:48:05 -0700

    JS currying

---
+++ js/curry.js
@@ -0,0 +1,10 @@
+import assert from "assert/strict";
+
+const f = (x, y) => x + y;
+const g = (x) => (y) => x + y;
+
+assert(f(5, 13) == 18); // Pass arguments together
+assert(g(5)(13) == 18); // Pass arguments one at a time
+
+const addFive = g(5); // Partial application
+assert(addFive(13) == 18);
