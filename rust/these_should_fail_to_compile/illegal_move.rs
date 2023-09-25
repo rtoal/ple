37fe18ccfdadd5400f8a28a82b5670e672910934
Ray Toal <rtoal@lmu.edu>
2023-09-25 07:44:30 -0700

    Move out some of the under construction Rust examples

--- rust/these_should_fail_to_compile/illegal_move.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let a = vec![0, 1, 2];  // owned by variable a
-    let b = a;              // owned by variable b
-    println!("{:?}", a);    // WILL NOT COMPILE
-}
--- rust/underconstruction/borrow.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let mut a = 5;
-    let ref c = a;
-    borrow(&mut a);
-    println!("Here is a: {}", a);
-}
-
-fn borrow(b: &mut u8) {
-}
