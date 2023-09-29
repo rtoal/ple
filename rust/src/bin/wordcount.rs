d048ae48caca8665fc8a063a5f73d59e2b15519f
Ray Toal <rtoal@lmu.edu>
2023-09-29 12:26:56 -0700

    Move Rust wordcount from underconstruction to main folder

--- rust/src/bin/wordcount.rs
+++ rust/src/bin/wordcount.rs
@@ -6,21 +6,20 @@ use std::io::{self, BufRead};
 use regex::Regex;
 
 fn main() {
-    let word_re = Regex::new(r"[\pL']+").unwrap();
+    let word_pattern = Regex::new(r"[\pL']+").unwrap();
 
     let mut counts: BTreeMap<String, isize> = BTreeMap::new();
 
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
+    for line in io::stdin().lock().lines() {
         let line = line.unwrap().to_lowercase();
-        let matches = word_re.find_iter(&line);
-        let words = matches.map(|(x, y)| &line[x..y]);
+        let matches = word_pattern.find_iter(&line);
+        let words: Vec<&str> = matches.map(|m| m.as_str()).collect();
 
         for word in words {
             *counts.entry(word.into()).or_insert(0) += 1;
         }
     }
     for (key, value) in counts.iter() {
-        println!("{} {}", key, value);
+        println!("{key} {value}");
     }
 }
