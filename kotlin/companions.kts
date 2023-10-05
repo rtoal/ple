84be92d9a301c8acdffa537641de3e531f64c999
Ray Toal <rtoal@lmu.edu>
2023-10-05 11:06:15 -0700

    Better Kotlin companion object example

--- kotlin/companions.kts
+++ kotlin/companions.kts
@@ -1,4 +1,5 @@
 import kotlin.math.hypot
+import kotlin.math.sqrt
 
 data class Point (val x: Double, val y: Double) {
     companion object {
@@ -6,10 +7,14 @@ data class Point (val x: Double, val y: Double) {
         fun midpoint(p: Point, q: Point) = Point(
             (p.x + q.x) / 2.0, (p.y + q.y) / 2.0)
     }
-    fun distanceFromOrigin() = distance(ORIGIN)
-    fun distance(p: Point) = hypot(x - p.x, y - p.y)
+    fun distanceFromOrigin() = ORIGIN distanceTo this
+    fun mirror() = Point(-x, -y)
+    infix fun distanceTo(p: Point) = hypot(x - p.x, y - p.y)
 }
- 
-println(Point.midpoint(Point.ORIGIN, Point(1.0, 1.0)))
-println(Point(1.0, 1.0).distanceFromOrigin())
-println(Point(1.0, 1.0).distance(Point(0.0, 0.0)))
+
+val oneOne = Point(1.0, 1.0)
+val halfHalf = Point(0.5, 0.5)
+assert(Point.midpoint(Point.ORIGIN, oneOne) == halfHalf)
+assert(Point(4.0, -3.0).distanceFromOrigin() == 5.0)
+assert(Point(1.0, 1.0) distanceTo Point(0.0, 0.0) == sqrt(2.0))
+assert(oneOne.mirror() == Point(-1.0, -1.0))
