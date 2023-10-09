b191be54d7be943225be0c748ff2aaf236cd3e4a
Ray Toal <rtoal@lmu.edu>
2023-10-09 15:16:24 -0700

    Reorganize Java folder

--- java/test.sh
+++ java/test.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-javac *.java && \
-javac a/*.java && \
-javac b/*.java && \
+javac -cp .:json.jar PokeApiApp.java *.java && \
 javac old/*.java && \
 java -ea AnagramApp rats | diff ../test/rats_heap_expected - && \
 java -ea AnimalChorusExample && \
 java -ea AnimalsApp && \
 java -ea ArithmeticSequenceExample && \
+java -ea BillionDollarMistakeExample && \
 java -ea CircleApp && \
 java -ea EqualsExample && \
 java -ea EvenSquareSummer && \
@@ -14,10 +13,11 @@ java -ea IntrospectionExample && \
 java -ea InvarianceExample && \
 java -ea ListAndSetExample && \
 java -ea MinMaxTester && \
-java -ea OptionalDemo && \
+java -ea OptionalExample && \
 java -ea PairExample && \
 java -ea PermutationsApp I like carrots | diff ../test/carrots_expected - && \
-java -ea PrimePrinter && \
+java -cp .:json.jar PokeApiApp && \
+java -ea PrimePrinterApp && \
 java -ea RaceConditionExample && \
 java -ea ReflectionExample && \
 java -ea SheepAdder && \
@@ -26,6 +26,7 @@ java -ea TraditionalWordCountApp < ../test/wordcount_ascii_input | diff ../test/
 java -ea TripleApp | diff ../test/triple_expected - && \
 java -ea TwiceExample && \
 java -ea TypeExperiment && \
+java -ea VariableExample && \
 java -ea WordCountApp < ../test/wordcount_ascii_input | diff ../test/wordcount_ascii_expected -
 
 if [ $? -ne 0 ]; then
