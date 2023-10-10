f4e36695c6a04e5ca779965d240b05e95505b1f9
Ray Toal <rtoal@lmu.edu>
2023-10-09 19:19:19 -0700

    Julia top ten scorers

---
+++ julia/top_ten_scorers.jl
@@ -0,0 +1,29 @@
+using Printf
+
+struct Player
+    team::String
+    name::String
+    games::Int
+    ppg::Float64
+end
+
+function Player(line::Vector{SubString{String}})
+    points = parse(Float64, line[4])
+    games = parse(Int, line[3])
+    return Player(line[1], line[2], games, points / games)
+end
+
+reportLine(p::Player) = @sprintf(
+    "%-22s%-4s%8.2f", p.name, p.team, p.ppg)
+
+players = Vector{Player}()
+for line in eachline(stdin)
+    player = Player(split(line, ","))
+    if player.games >= 15
+        push!(players, player)
+    end
+end
+sort!(players, by=player -> player.ppg, rev=true)
+for player in players[1:10]
+    println(reportLine(player))
+end
