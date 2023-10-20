ef757886303715e58bd3dcc4ad8d3fbada924c58
Ray Toal <rtoal@lmu.edu>
2023-10-20 13:42:46 -0700

    Fibonacci with sparks

--- haskell/fib.hs
+++ haskell/fib.hs
@@ -2,9 +2,9 @@ import GHC.Conc
 
 fib :: Int -> Int
 fib n
-    | n <= 1 = 1
-    | otherwise = par a (seq b (a + b + 1))
+    | n <= 1 = n
+    | otherwise = par a (pseq b (a + b))
                      where a = fib (n-1)
                            b = fib (n-2)
 
-main = print (fib 46)
+main = print (fib 45)
