3d7086009cca596116e03b1905a40b39c64072e4
Ray Toal <rtoal@lmu.edu>
2023-11-05 19:37:47 -0800

    C Strings

--- c/strings.c
+++ c/strings.c
@@ -1,10 +1,18 @@
-// Examples of various string functions
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main() {
-    char s[25] = "Hello";
-    // TODO
+    char s[25];                         // large buffer, ok
+    s[0] = 'H';
+    s[1] = 'i';
+    s[2] = '!';
+    s[3] = '\x00';                      // zero terminator!
+    assert(strlen(s) == 3);             // length of string
+    assert(strcmp(s, "Hi!") == 0);      // compare (equal)
+    assert(strcmp(s, "Hat") < 0);       // compare (less)
+    assert(strcmp(s, "Yo") > 0);        // compare (greater)
+    assert(strstr(s, "i!") == s + 1);   // find substring
+    assert(strstr(s, "x") == NULL);
+    assert(strchr(s, '!') == s + 2);    // find character
 }
