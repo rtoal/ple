5984bc7f661e5d56c239a429a1ca54329eb3b6c9
umamigabi <123357590+gabrielle-barnes@users.noreply.github.com>
2023-12-17 18:23:47 -0800

    zig triple code

--- zig/coords.zig
+++ zig/coords.zig
@@ -1,9 +1,7 @@
 const std = @import("std");
-//importing standard zig library
 const print = std.debug.print;
 
 fn coord(x: i32, y: i32, z: i32) void {
-    //the coord function is taking three i32 arguments, and the return type is void
     print("The coordinates are: x = {}, y = {}, z = {}\n", .{ x, y, z });
 }
 
