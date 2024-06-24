a479874f5beebeb789af9c8caefb8650f7750a2f
Ray Toal <rtoal@lmu.edu>
2024-06-24 13:49:39 -0700

    Perl word count

--- perl/test.sh
+++ perl/test.sh
@@ -4,6 +4,7 @@ perl summer.pl && \
 perl triple.pl | diff ../test/triple_expected - && \
 perl top_ten_scorers.pl < ../test/wnba_input | diff ../test/wnba_expected - && \
 perl twice.pl
+perl wordcount.pl < ../test/wordcount_input | diff ../test/wordcount_expected - && \
 
 if [ $? -ne 0 ]; then
     echo
