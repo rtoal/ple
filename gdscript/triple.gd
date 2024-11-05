75475f1ea3c1e95a560c8db6e7d7880766319411
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2024-11-05 03:54:28 -0800

    Now works when running on command line

--- gdscript/triple.gd
+++ gdscript/triple.gd
@@ -1,5 +1,12 @@
-for c in range(1, 41):
-	for b in range(1, c):
-		for a in range(1, b):
-			if a * a + b * b == c * c:
-				print("%d, %d, %d" % [a, b, c])
+extends SceneTree
+
+func triple():
+	for c in range(1, 41):
+		for b in range(1, c):
+			for a in range(1, b):
+				if a * a + b * b == c * c:
+					print("%d, %d, %d" % [a, b, c])
+
+func _init():
+	triple()
+	quit()
