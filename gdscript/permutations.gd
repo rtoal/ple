ccc38583b10ec5067a2604e67e321056ca46d184
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2024-11-14 00:36:39 -0800

    Added permutations

---
+++ gdscript/permutations.gd
@@ -0,0 +1,21 @@
+extends SceneTree
+
+func print_permutations(a, n):
+	if n <= 0:
+		print("\t".join(a))
+	else:
+		for i in range(n):
+			print_permutations(a, n - 1)
+			var j = 0 if n % 2 == 0 else i
+			var temp = a[j] 
+			a[j] = a[n] 
+			a[n] = temp 
+		print_permutations(a, n - 1)
+
+func _init():
+	var args = Array(OS.get_cmdline_args())
+	if len(args) < 3:
+		print("")
+	else:
+		print_permutations(args.slice(2), len(args.slice(2)) - 1)
+	quit()
