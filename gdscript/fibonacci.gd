97278db245ec146a07ac48425d2aba9291a6817e
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2024-11-14 00:11:06 -0800

    Added fibonacci for recursion

---
+++ gdscript/fibonacci.gd
@@ -0,0 +1,22 @@
+extends SceneTree
+
+func fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+func _init():
+    assert(fibonacci(0) == 0)
+    assert(fibonacci(1) == 1)
+    assert(fibonacci(2) == 1)
+    assert(fibonacci(3) == 2)
+    assert(fibonacci(4) == 3)
+    assert(fibonacci(5) == 5)
+    assert(fibonacci(6) == 8)
+    assert(fibonacci(7) == 13)
+    assert(fibonacci(8) == 21)
+    assert(fibonacci(20) == 6765)
+    quit()
+
