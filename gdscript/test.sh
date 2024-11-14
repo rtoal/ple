7947eaa0c35e09d84712068e6485d1251171373c
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2024-11-14 01:16:24 -0800

    Added new tests for bash testing

--- gdscript/test.sh
+++ gdscript/test.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+godot --display-driver headless -s anagrams.gd | dif - ../test/rats_heap_expected && \
+godot --display-driver headless -s file_reader.gd ../test/carrots_expected | diff ../test/carrots_expected && \
+godot --display-driver headless -s fibonacci.gd && \
 godot --display-driver headless -s hello_world.gd && \
+godot --display-driver headless -s permutations.gd I like carrots | diff - ../test/carrots_expected && \
+godot --display-driver headless -s sum_of_even_squares && \
 godot --display-driver headless -s triple.gd | diff ../test/triple_expected - && \
 godot --display-driver headless -s vectors.gd && \
 
