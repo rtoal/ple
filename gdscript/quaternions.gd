1a54b28ec7509d05d9d3708dfc009f37ff363977
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2024-11-30 20:12:14 -0800

    Reedited Quaternions to make them work

--- gdscript/quaternions.gd
+++ gdscript/quaternions.gd
@@ -1,17 +1,18 @@
 extends SceneTree
 
 func _init():
-    var q1 = Quaternion(1.0, 2.0, 3.0, 4.0)
-    var q2 = Quaternion()	# Quaternion(0, 0, 0, 1)
-    var q3 = q1.inverse()   # Quaternion(-1, -2, -3, 4)
-    var q4 = Quaternion(5.0, 2.0, 8.0, 9.0)
-    var q5 = q1 + q4        # Quaternion(6, 4, 11, 13)
-    var q6 = q1 * q4        # Quaternion(39, 33, 51, 3)
+	var q1 = Quaternion(1.0, 2.0, 3.0, 4.0)
+	var q2 = Quaternion()					# Quaternion(0, 0, 0, 1)
+	var q3 = q1.normalized()				# Quaternion(0.182574, 0.365148, 0.547723, 0.730297)
+	var q4 = Quaternion(5.0, 2.0, 8.0, 9.0)
+	var q5 = q1 + q4						# Quaternion(6, 4, 11, 13)
+	var q6 = q1 * q4						# Quaternion(39, 33, 51, 3)
 
-    assert(q5 == Quaternion(6, 4, 11, 13))
-    assert(q2 != q5)
-    assert(q3 == Quaternion(-1, -2, -3, 4))
-    assert(q6 == Quaternion(39, 33, 51, 3))
+	assert(q5 == Quaternion(6, 4, 11, 13))
+	assert(q2 != q5)
+	assert(q3.is_equal_approx(Quaternion(0.182574, 0.365148, 0.547723, 0.730297)))
+	assert(q3.inverse().is_equal_approx(Quaternion(-0.182574, -0.365148, -0.547723, 0.730297)))
+	assert(q6 == Quaternion(39, 33, 51, 3))
 
 
-    quit()
+	quit()
--- gdscript/test.ps1
+++ gdscript/test.ps1
@@ -9,15 +9,26 @@ function Assert-MatchTests {
 }
 
 $Error.clear()
-godot --display-driver headless --no-header -s "$PSScriptRoot\anagrams.gd" rats | Compare-Object (Get-Content "$PSScriptRoot\..\test\rats_heap_expected") | Assert-MatchTests &&
+godot --display-driver headless --no-header -s "$PSScriptRoot\anagrams.gd" rats | 
+    Compare-Object (Get-Content "$PSScriptRoot\..\test\rats_heap_expected") | 
+    Assert-MatchTests &&
 godot --display-driver headless --no-header -s "$PSScriptRoot\fibonacci.gd" &&
-godot --display-driver headless --no-header -s "$PSScriptRoot\file_reader.gd" "$PSScriptRoot\..\test\carrots_expected" | Compare-Object (Get-Content "$PSScriptRoot\..\test\carrots_expected") | Assert-MatchTests &&
+godot --display-driver headless --no-header -s "$PSScriptRoot\file_reader.gd" "$PSScriptRoot\..\test\carrots_expected" | 
+    Compare-Object (Get-Content "$PSScriptRoot\..\test\carrots_expected") | 
+    Assert-MatchTests &&
 godot --display-driver headless --no-header -s "$PSScriptRoot\hello_world.gd" &&
-godot --display-driver headless --no-header -s "$PSScriptRoot\permutations.gd" I like carrots | Compare-Object (Get-Content "$PSScriptRoot\..\test\carrots_expected") | Assert-MatchTests &&
+#godot --display-driver headless --no-header -s "$PSScriptRoot\lcs.gd" "$PSScriptRoot\..\test\lcs_input" | 
+#    Compare-Object (Get-Content "$PSScriptRoot\..\test\lcs_expected") | 
+#    Assert-MatchTests &&
+godot --display-driver headless --no-header -s "$PSScriptRoot\permutations.gd" I like carrots | 
+    Compare-Object (Get-Content "$PSScriptRoot\..\test\carrots_expected") | 
+    Assert-MatchTests &&
 godot --display-driver headless --no-header -s "$PSScriptRoot\quaternions.gd" &&
 godot --display-driver headless --no-header -s "$PSScriptRoot\static_variables.gd" &&
 godot --display-driver headless --no-header -s "$PSScriptRoot\sum_of_even_squares.gd" &&
-godot --display-driver headless --no-header -s "$PSScriptRoot\triple.gd" | Compare-Object (Get-Content "$PSScriptRoot\..\test\triple_expected") | Assert-MatchTests &&
+godot --display-driver headless --no-header -s "$PSScriptRoot\triple.gd" | 
+    Compare-Object (Get-Content "$PSScriptRoot\..\test\triple_expected") | 
+    Assert-MatchTests &&
 godot --display-driver headless --no-header -s "$PSScriptRoot\vectors.gd" &&
 ForEach-Object 'foo';
 
