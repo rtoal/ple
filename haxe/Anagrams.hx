40b371162c6e06c0ef2b92a0b097f3a6fe588022
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2024-12-09 23:56:00 -0800

    Added Anagrams

---
+++ haxe/Anagrams.hx
@@ -0,0 +1,24 @@
+class Anagrams {
+  static function generatePermutations(a: Array<String>, n: Int):Void {
+    if (n == 0) {
+      haxe.Log.trace(a.join(""), null);
+    } else {
+      for (i in 0...n) {
+        generatePermutations(a, n - 1);
+        var j:Int = (n % 2 == 0) ? 0 : i;
+        var temp:String = a[j];
+        a[j] = a[n];
+        a[n] = temp;
+      }
+      generatePermutations(a, n-1);
+    }
+  }
+
+  static public function main():Void {
+    if (Sys.args().length != 1) {
+      throw "Exactly one argument is required";
+    }
+    var word:Array<String> = Sys.args()[0].split("");
+    generatePermutations(word, word.length-1);
+  }
+}
