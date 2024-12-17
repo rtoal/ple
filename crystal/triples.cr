bbc958b57b80f62af269ac2c781f78bda59f6dd1
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2024-12-17 02:37:32 -0800

    Updated triple

--- crystal/triples.cr
+++ crystal/triples.cr
@@ -1,6 +1,6 @@
-(1..40).each do |c|
-  (1..c).each do |b|
-    (1..b).each do |a|
+1.upto 40 do |c|
+  1.upto c do |b|
+    1.upto b do |a|
       if a ** 2 + b ** 2 == c ** 2
         puts "#{a}, #{b}, #{c}"
       end
