b5bc89a616c38a398cb2ef8b28057dc8dab9e817
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2024-12-25 18:55:35 -0800

    Added anagrams and factorial gen

--- neko/test.ps1
+++ neko/test.ps1
@@ -16,12 +16,16 @@ if (!(Test-Path -Path $bin)) {
 } 
 
 $Error.clear()
+nekoc -o $bin "$PSScriptRoot\anagrams.neko" && "rats" | neko "$bin\anagrams.n" |
+    Compare-Object (Get-Content "$PSScriptRoot\..\test\rats_heap_expected") |
+    Assert-MatchTests &&
 nekoc -o $bin "$PSScriptRoot\clockhands_date.neko" && neko "$bin\clockhands_date.n" |
     Compare-Object (Get-Content "$PSScriptRoot\..\test\clockhands_expected") |
     Assert-MatchTests &&
 nekoc -o $bin "$PSScriptRoot\clockhands.neko" && neko "$bin\clockhands.n" |
     Compare-Object (Get-Content "$PSScriptRoot\..\test\clockhands_expected") |
     Assert-MatchTests &&
+nekoc -o $bin "$PSScriptRoot\factorial_generator.neko" && neko "$bin\factorial_generator.n" &&
 nekoc -o $bin "$PSScriptRoot\hello_world.neko" && neko "$bin\hello_world.n" &&
 nekoc -o $bin "$PSScriptRoot\triple.neko" && neko "$bin\triple.n" |
     Compare-Object (Get-Content "$PSScriptRoot\..\test\triple_expected") |
