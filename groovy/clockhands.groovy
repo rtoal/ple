3dc1afebac83c5343dc0365ce2c7a62389dfc562
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2024-12-31 19:12:27 -0800

    Improved clockhands example

--- groovy/clockhands.groovy
+++ groovy/clockhands.groovy
@@ -1,6 +1,6 @@
 0.upto(10) {i ->
-    def t = (int)((i + 0.5) * 43200.0 / 11);
-    def h = t.intdiv(3600);
-    def m = t % 3600;
-    printf("%02d:%02d:%02d\n", (h == 0 ? 12 : h), m.intdiv(60), m % 60);
+    int t = (i + 0.5) * 43200.0 / 11
+    int h = t / 3600
+    int m = t % 3600
+    printf "%02d:%02d:%02d\n", h == 0 ? 12 : h, m.intdiv(60), m % 60
 }
