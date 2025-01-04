bb9659c083c3c3f43df13923e7603d158ba55c9c
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-03 17:41:17 -0800

    Added anagrams/permutations

--- io/test.ps1
+++ io/test.ps1
@@ -3,12 +3,15 @@ function Assert-MatchTests {
         [Parameter(Mandatory = $true, ValueFromPipeline)] $TestResult
     )
 
-    if ($null -ne $TestResult) {
+    if ($TestResult) {
         Write-Error "Output does not match expected results."
     }
 }
 
 $Error.clear()
+io "$PSScriptRoot\anagrams.io" rats |
+    Compare-Object (Get-Content "$PSScriptRoot\..\test\rats_heap_expected") |
+    Assert-MatchTests &&
 io "$PSScriptRoot\cj.io" &&
 io "$PSScriptRoot\clockhands.io" |
     Compare-Object (Get-Content "$PSScriptRoot\..\test\clockhands_expected") |
@@ -17,6 +20,11 @@ io "$PSScriptRoot\clockhandsOneLine.io" |
     Compare-Object (Get-Content "$PSScriptRoot\..\test\clockhands_expected") |
     Assert-MatchTests &&
 io "$PSScriptRoot\hello.io" &&
+io "$PSScriptRoot\permutations.io" I like carrots | 
+    Compare-Object (Get-Content "$PSScriptRoot\..\test\carrots_expected") |
+    Assert-MatchTests &&
+io "$PSScriptRoot\string_interpreter.io" &&
+io "$PSScriptRoot\sumOfEvenSquares.io" &&
 io "$PSScriptRoot\triple.io" |
     Compare-Object (Get-Content "$PSScriptRoot\..\test\triple_expected") |
     Assert-MatchTests &&
