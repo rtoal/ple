526290df6b06ed838677ea2d7fe2444321a34ae2
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-06 12:51:35 -0800

    Added sum of even squares

---
+++ powershell/sum_of_even_squares.ps1
@@ -0,0 +1,25 @@
+function Assert-Equals {
+    param (
+        [Parameter(Mandatory = $true)] $Item1,
+        [Parameter(Mandatory = $true)] $Item2
+    )
+    
+    if ($Item1 -ne $Item2) {
+        Write-Error "$Item1 and $Item2 are not equals."
+    }
+}
+
+function Get-Sum-Of-Even-Squares {
+    param (
+        [Parameter(Mandatory = $true)] $Numbers
+    )
+    
+    filter Even {if ($_ % 2 -eq 0) {$_ * $_}}
+    $Numbers | Even | ForEach-Object {$sum=0} {$sum += $_} {$sum}
+}
+
+Assert-Equals (Get-Sum-Of-Even-Squares (@())) (0)
+Assert-Equals (Get-Sum-Of-Even-Squares (@(1))) (0)
+Assert-Equals (Get-Sum-Of-Even-Squares (1..2)) (4)
+Assert-Equals (Get-Sum-Of-Even-Squares (1..3)) (4)
+Assert-Equals (Get-Sum-Of-Even-Squares (1..4)) (20)
