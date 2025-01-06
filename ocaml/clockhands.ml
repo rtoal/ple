0f5a5b2bf9835acdd6a7931976eb4c4fe38800bb
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-05 21:07:10 -0800

    Added clockhands example (from Dr. Toal's clockhands examples)

---
+++ ocaml/clockhands.ml
@@ -0,0 +1,6 @@
+for i = 0 to 10 do
+  let t = truncate ((float i +. 0.5) *. 43200.0 /. 11.0) in
+  let h = t / 3600 in
+  let m = t mod 3600 in
+  Printf.printf ("%02d:%02d:%02d\n") (if h = 0 then 12 else h) (m / 60) (m mod 60)
+done;;
