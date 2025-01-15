fea838adc920be127a53c8acd88f0404b1e48863
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-14 18:04:17 -0800

    New Best (14)

--- pyth/test.ps1
+++ pyth/test.ps1
@@ -12,13 +12,16 @@ function Assert-MatchTests {
 $path = "$PSScriptRoot\pyth\pyth.py"
 
 $Error.clear()
+"rats" | python .\pyth\pyth.py -c (Get-Content "$PSScriptRoot\anagrams.pyth") | 
+    Compare-Object (Get-Content "$PSScriptRoot\..\test\rats_recursive_expected") |
+    Assert-MatchTests &&
 python $path -c (Get-Content "$PSScriptRoot\clockhands.pyth") | 
     Compare-Object (Get-Content "$PSScriptRoot\..\test\clockhands_expected") |
     Assert-MatchTests &&
 python $path -c (Get-Content "$PSScriptRoot\first_twelve_fibs.pyth") &&
 python $path -c (Get-Content "$PSScriptRoot\five_hellos.pyth") &&
 python $path -c (Get-Content "$PSScriptRoot\hello.pyth") &&
-# python $path -c (Get-Content "$PSScriptRoot\hypot.pyth") &&
+# python $path -c (Get-Content "$PSScriptRoot\hypot.pyth") 4 &&
 python $path -c (Get-Content "$PSScriptRoot\triangle.pyth") &&
 python $path -c (Get-Content "$PSScriptRoot\triple.pyth") | 
     Compare-Object (Get-Content "$PSScriptRoot\..\test\triple_expected") |
