cce9abfc7884551d3c876ba8cf1025494a531de5
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-15 19:29:23 -0800

    Added v clockhands

--- v/test.ps1
+++ v/test.ps1
@@ -12,6 +12,9 @@ $Error.clear()
 v run "$PSScriptRoot\anagrams.v" rats |
     Compare-Object (Get-Content "$PSScriptRoot\..\test\rats_heap_expected") |
     Assert-MatchTests &&
+v run "$PSScriptRoot\clockhands.v" |
+    Compare-Object (Get-Content "$PSScriptRoot\..\test\clockhands_expected") |
+    Assert-MatchTests &&
 v run "$PSScriptRoot\triple.v" |
     Compare-Object (Get-Content "$PSScriptRoot\..\test\triple_expected") |
     Assert-MatchTests &&
