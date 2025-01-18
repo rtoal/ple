a7deecf1ed42fa5c6a272b2b83e96b74c85c5d95
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-18 13:01:48 -0800

    Edited clockhands (improved by Toal)

--- v/clockhands.v
+++ v/clockhands.v
@@ -1,8 +1,7 @@
 fn main() {
     for i in 0 .. 11 {
         t := int((i + 0.5) * 43200 / 11)
-        h := int(t / 3600)
-        m := t % 3600
-        println('${(if h == 0 { 12 } else { h }):02}:${int(m / 60):02}:${(m % 60):02}')
+        h, m, s := int(t / 3600), int(t / 60) % 60, t % 60
+        println('${(if h == 0 { 12 } else { h }):02}:${m:02}:${s:02}')
     } 
 }
