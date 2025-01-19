ebf5e31410a817d043987e4be860b3d507bd93f5
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-18 17:53:29 -0800

    Added loop value

--- neko/test.ps1
+++ neko/test.ps1
@@ -27,6 +27,7 @@ nekoc -o $bin "$PSScriptRoot\clockhands.neko" && neko "$bin\clockhands.n" |
     Assert-MatchTests &&
 nekoc -o $bin "$PSScriptRoot\factorial_generator.neko" && neko "$bin\factorial_generator.n" &&
 nekoc -o $bin "$PSScriptRoot\hello_world.neko" && neko "$bin\hello_world.n" &&
+nekoc -o $bin "$PSScriptRoot\loop_value.neko" && neko "$bin\loop_value.n" &&
 nekoc -o $bin "$PSScriptRoot\permutations.neko" && "I like carrots" | neko "$bin\permutations.n" |
     Compare-Object (Get-Content "$PSScriptRoot\..\test\carrots_expected") |
     Assert-MatchTests &&
