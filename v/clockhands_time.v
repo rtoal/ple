4327ebf71d7bbcbb7f0cd74429b18cbea4748985
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-20 01:55:39 -0800

    Revised for brevity

--- v/clockhands_time.v
+++ v/clockhands_time.v
@@ -3,7 +3,6 @@ import time
 fn main() {
     for i in 0 .. 11 {
         t := (43200 * i + 21600) / 11
-        clock := time.unix(t)
-        println(clock.custom_format("hh:mm:ss"))
+        println(time.unix(t).custom_format("hh:mm:ss"))
     } 
 }
