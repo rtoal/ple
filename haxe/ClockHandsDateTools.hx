a50f1bf1188143f98a9a4d94e16e15c7df468485
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-20 15:55:54 -0800

    Revised to account of timezone offset

--- haxe/ClockHandsDateTools.hx
+++ haxe/ClockHandsDateTools.hx
@@ -2,7 +2,9 @@ using DateTools;
 class ClockHandsDateTools {
   public static function main():Void {
     for (i in 0...11){
-      haxe.Log.trace((DateTools.format(Date.fromTime((i + 0.5) * 43200000 / 11 + 3600000 * 8), "%I:%M:%S")), null);
+      haxe.Log.trace((DateTools.format(
+        Date.fromTime((i + 0.5) * 43200000 / 11 + Date.now().getTimezoneOffset() * 60000), "%I:%M:%S")), 
+        null);
     }
   }
 }
