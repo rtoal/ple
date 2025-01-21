2410a8dbd99e41a896aab503a3c8b75d86b6f7e9
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-20 17:07:09 -0800

    Space -> Tabs Indent

--- go/clockhands_date.go
+++ go/clockhands_date.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-    for i := 0; i < 11; i++ {
-        var t int64 = int64((43200 * i + 21600) / 11)
-        fmt.Println(time.Unix(t, 0).UTC().Format("03:04:05"))
-    }
+	for i := 0; i < 11; i++ {
+		var t int64 = int64((43200 * i + 21600) / 11)
+		fmt.Println(time.Unix(t, 0).UTC().Format("03:04:05"))
+	}
 }
