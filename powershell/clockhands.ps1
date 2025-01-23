40e7236256d364b7c635964fd39e2458ddb2ba52
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-22 17:20:21 -0800

    Edited clockhands (Added clockhands for R)

--- powershell/clockhands.ps1
+++ powershell/clockhands.ps1
@@ -1,6 +1,7 @@
 foreach ($i in 0..10) {
-    $t = [int][Math]::Floor(($i + 0.5) * 43200 / 11)
+    $t = [int][Math]::Floor((43200 * $i + 21600) / 11)
     $h = [int][Math]::Floor($t / 3600)
-    $m = $t % 3600
-    "{0:D2}:{1:D2}:{2:D2}" -f (($h -eq 0)? 12 : $h), [int][Math]::Floor($m / 60), ($m % 60)
+    $m = [int][Math]::Floor($t / 60) % 60
+    $s = $t % 60
+    "{0:D2}:{1:D2}:{2:D2}" -f (($h -eq 0)? 12 : $h), $m, $s
 }
