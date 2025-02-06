5682c78d66637521da5e3bc3bf01f2fb19d5f6e2
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-02-06 04:05:19 -0800

    Revised clockhands

--- zig/clockhands.zig
+++ zig/clockhands.zig
@@ -1,10 +1,11 @@
-const bufPrint = @import("std").debug.print;
+const print = @import("std").debug.print;
 
 pub fn main() void {
     for (0..11) |i| {
-        const t = (i * 43200 + 21600) / 11;
+        const t = (43200 * i + 21600) / 11;
         const h = t / 3600;
-        const m = t % 3600;
-        bufPrint("{d:0>2}:{d:0>2}:{d:0>2}\n", .{ if (h == 0) 12 else h, m / 60, m % 60 });
+        const m = t / 60 % 60;
+        const s = t % 60;
+        print("{d:0>2}:{d:0>2}:{d:0>2}\n", .{ if (h == 0) 12 else h, m, s });
     }
 }
