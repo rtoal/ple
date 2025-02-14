e4d9eebc3aaf2a62b5fa905f0d3b07767c173278
Ray Toal <rtoal@lmu.edu>
2025-02-13 16:52:58 -0800

    Bye bye old browser

--- docs/resources/scripts/overviews.js
+++ docs/resources/scripts/overviews.js
@@ -1,6 +1,13 @@
-var languageNames = langs.map((lang) => lang.i);
+const nav = document.querySelector("nav");
 
-var iconFor = function (iconName, uri) {
+for (let lang of langs) {
+  const item = document.createElement("div");
+  item.appendChild(document.createTextNode(lang.n));
+  item.onclick = () => show(lang);
+  nav.appendChild(item);
+}
+
+function iconFor(iconName, uri) {
   return (
     "<a href=" +
     uri +
@@ -8,11 +15,7 @@ var iconFor = function (iconName, uri) {
     iconName +
     "-icon.png height=20></a>"
   );
-};
-
-document.getElementById("index").onclick = () => {
-  window.location.href = "";
-};
+}
 
 langs.forEach((lang, index) => {
   lang.index = index;
@@ -22,10 +25,7 @@ langs.forEach((lang, index) => {
   span.data = lang;
   span.setAttributeNode(attribute);
   span.style.backgroundImage = "url(resources/" + lang.i + "-logo-64.png)";
-  document.getElementById("content").appendChild(span);
-  span.onclick = function () {
-    show(lang);
-  };
+  document.querySelector("#index").appendChild(span);
   if ((lang.h || lang.w || lang.g || lang.rc || lang.pp) && lang.d) {
     var icons = [];
     if (lang.h) icons.push(iconFor("home", lang.h));
@@ -62,20 +62,15 @@ langs.forEach((lang, index) => {
   }
 });
 
-var imageElement = document.getElementById("logo");
+var imageElement = document.querySelector("#logo img");
 var description = document.getElementById("description");
 var info = document.getElementById("info");
 var tags = document.getElementById("tags");
-var prev = document.getElementById("prev");
-var next = document.getElementById("next");
 
-var show = function (language) {
-  document.getElementById("content").style.display = "none";
-  document.getElementById("single-language").style.display = "block";
-  document.getElementById("bar").innerHTML = language.n;
-
-  imageElement.style.backgroundImage =
-    "url(resources/" + language.i + "-logo-240.png)";
+function show(language) {
+  document.querySelector("#index").style.display = "none";
+  imageElement.src = "resources/" + language.i + "-logo-240.png";
+  imageElement.alt = `Logo for ${language.n}`;
   if (language.d) {
     description.innerHTML = language.d;
     description.style.display = "block";
@@ -113,44 +108,4 @@ var show = function (language) {
   } else {
     tags.style.display = "none";
   }
-
-  var prevIndex = (language.index + langs.length - 1) % langs.length;
-  var nextIndex = (language.index + 1) % langs.length;
-  prev.innerHTML = "&larr; " + langs[prevIndex].n;
-  next.innerHTML = langs[nextIndex].n + " &rarr;";
-  prev.onclick = () => show(langs[prevIndex]);
-  next.onclick = () => show(langs[nextIndex]);
-  window.location.hash = "#" + language.i;
-};
-
-function home() {
-  window.location.hash = "";
-  document.getElementById("single-language").style.display = "none";
-  document.getElementById("content").style.display = "block";
-  document.getElementById("bar").innerHTML = "Programming Languages";
 }
-
-addEventListener("keydown", function (event) {
-  var key = window.location.hash.substring(1);
-  var index = languageNames.indexOf(key);
-  if (isNaN(index) || index < 0 || index >= langs.length) {
-    return;
-  }
-  if (event.keyCode === 37) {
-    show(langs[(index + langs.length - 1) % langs.length]);
-  } else if (event.keyCode === 39) {
-    show(langs[(index + 1) % langs.length]);
-  } else if (event.keyCode == 38) {
-    home();
-  }
-});
-
-window.onload = window.onpopstate = function () {
-  var key = window.location.hash.substring(1);
-  var index = languageNames.indexOf(key);
-  if (index >= 0) {
-    show(langs[index]);
-  } else {
-    home();
-  }
-};
