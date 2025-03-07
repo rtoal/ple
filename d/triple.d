c15aabe17defa45b38fadf7632ddc0383814930c
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-03-07 00:06:23 -0800

    Added triple example

---
+++ d/triple.d
@@ -0,0 +1,14 @@
+import core.stdc.stdio;
+
+int main() {
+    for (int c = 0; c < 41; c++) {
+        for (int b = 0; b < c; b++) {
+            for (int a = 0; a < b; a++) {
+                if (a * a + b * b == c * c) {
+                    printf("%d, %d, %d\n", a, b, c);
+                }
+            }
+        }
+    }
+    return 0;
+}
