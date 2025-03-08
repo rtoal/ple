1bd5566eb83254a96b791e74771875d65e65276e
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-03-07 21:54:12 -0800

    Revised anagrams/permutations

--- grain/test.ps1
+++ grain/test.ps1
@@ -17,11 +17,17 @@ if (!(Test-Path -Path $bin)) {
 
 # This honestly could be better (bug with spaces in file path).
 $Error.clear()
+grain "`"$PSScriptRoot\anagrams.gr`"" -o "$bin\anagrams.gr.wasm" -- rats |
+    Compare-Object (Get-Content "$PSScriptRoot\..\test\rats_heap_expected") |
+    Assert-MatchTests &&
 grain "`"$PSScriptRoot\citiesList.gr`"" -o "$bin\citiesList.gr.wasm" &&
 grain "`"$PSScriptRoot\clockhands.gr`"" -o "$bin\clockhands.gr.wasm" |
     Compare-Object (Get-Content "$PSScriptRoot\..\test\clockhands_expected") |
     Assert-MatchTests &&
 grain "`"$PSScriptRoot\helloWorld.gr`"" -o "$bin\helloWorld.gr.wasm" && 
+grain "`"$PSScriptRoot\permutations.gr`"" -o "$bin\permutations.gr.wasm" -- rats |
+    Compare-Object (Get-Content "$PSScriptRoot\..\test\carrots_expected") |
+    Assert-MatchTests &&
 grain "`"$PSScriptRoot\triple.gr`"" -o "$bin\triple.gr.wasm" |
     Compare-Object (Get-Content "$PSScriptRoot\..\test\triple_expected") |
     Assert-MatchTests &&
