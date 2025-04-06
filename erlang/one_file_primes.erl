183305a8b8dfe031216d2a4b9dc589958486565e
Ray Toal <rtoal@lmu.edu>
2025-04-05 17:22:11 -0700

    Erlang improvements

--- erlang/one_file_primes.erl
+++ erlang/one_file_primes.erl
@@ -1,5 +1,15 @@
--module(prime_checker).
--export([is_prime/2]).
+% A command line application to print primes in 2..1000.
+main(_) ->
+  Max = 1000,
+  lists:foreach(
+    fun (N) ->
+      spawn(prime_checker, is_prime, [N, self()])
+    end,
+    lists:seq(2, Max)),
+  wait(Max-1).
+
+wait(0) -> io:format("~n");
+wait(N) -> receive _ -> wait(N-1) end.
 
 is_prime(N, Observer) ->
   (fun Check(D) ->
