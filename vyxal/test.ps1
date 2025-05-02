dfe1492fa4cc0a36d196315bd44cae74b61ffd14
unknown <wesleyng2003@gmail.com>
2025-05-02 09:51:36 -0700

    Added PowerShell test

---
+++ vyxal/test.ps1
@@ -0,0 +1,25 @@
+function Assert-MatchTests {
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline)] $TestResult
+    )
+    
+    if ($TestResult) {
+        Write-Error "Output does not match expected results."
+    }
+}
+
+# Location of the vyxal.jar file
+# You will need to readjust this line to fit with the name of the .jar file in this folder.
+$vyxal = "$PSScriptRoot\vyxal.jar"
+
+$Error.clear()
+java -jar $vyxal --file "$PSScriptRoot\codepage.vy" &&
+java -jar $vyxal --file "$PSScriptRoot\hello.vy" &&
+ForEach-Object 'foo';
+
+if ($Error -or !$?) { 
+    "*** VYXAL TESTS FAILED ***" 
+}
+else { 
+    "VYXAL TESTS PASSED" 
+} 
