8baeab213c201d1411fe73d7727ad10118fd81cd
unknown <wesleyng2003@gmail.com>
2025-05-16 09:50:23 -0700

    Shortened the code

--- visualbasic/ClockhandsTime.vb
+++ visualbasic/ClockhandsTime.vb
@@ -1,8 +1,7 @@
 Module ClockhandsTime
     Sub Main()
-        Dim noon = #12:00:00#
         For i = 0 To 10
-            Console.WriteLine(Format(noon.AddSeconds((43200 * i + 21600) / 11), "hh:mm:ss"))
+            Console.WriteLine(Format(#12:00:00#.AddSeconds((43200 * i + 21600) / 11), "hh:mm:ss"))
         Next 
     End Sub
 End Module
