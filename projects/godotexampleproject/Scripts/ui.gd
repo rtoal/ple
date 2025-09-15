d46adfcac097ebfed8ab90b71fb96981e51f45f4
Wesley Ng <wesleyng2003@gmail.com>
2025-09-15 14:41:46 -0700

    Added a RAINBOW SLIDER

--- projects/godotexampleproject/Scripts/ui.gd
+++ projects/godotexampleproject/Scripts/ui.gd
@@ -5,3 +5,11 @@ extends CanvasLayer
 # this function will play out.
 func _on_hello_button_pressed() -> void:
 	print("Hello!")
+
+func _on_color_slider_value_changed(red: float) -> void:
+	# Below, get_node("ColorSlider") is the same as $ColorSlider.
+	$ColorSlider.modulate = Color(red / 100, 0, 1)
+
+func _on_rainbow_slider_value_changed(h: float) -> void:
+	# Below, get_node("RainbowSlider") is the same as $RainbowSlider.
+	$RainbowSlider.modulate = Color.from_hsv(h / 100, 1, 1)
