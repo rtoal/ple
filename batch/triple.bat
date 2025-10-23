96218d99b3004734ae00b112ad6567cc6aa8fa70
Wesley Ng <wesleyng2003@gmail.com>
2025-10-23 12:41:54 -0700

    Improved triple

--- batch/triple.bat
+++ batch/triple.bat
@@ -1,19 +1,11 @@
 @ECHO OFF
-
-FOR /L %%c IN (1, 1, 40) DO CALL :FIRST %%c
-GOTO :END
-
-:FIRST
-FOR /L %%b IN (1, 1, %1) DO CALL :SECOND %1 %%b
-GOTO :END
-
-:SECOND
-FOR /L %%a IN (1, 1, %2) DO CALL :THIRD %1 %2 %%a
-GOTO :END
-
-:THIRD
-SET /A legs=%3 * %3 + %2 * %2
-SET /A hyp=%1*%1
-IF %legs% EQU %hyp% ECHO %3, %2, %1
-
-:END
+SETLOCAL ENABLEDELAYEDEXPANSION
+FOR /L %%c IN (1, 1, 40) DO (
+    FOR /L %%b IN (1, 1, %%c) DO (
+        FOR /L %%a IN (1, 1, %%b) DO (
+            SET /A legs=%%a * %%a + %%b * %%b
+            SET /A hyp=%%c * %%c
+            IF !legs! EQU !hyp! ECHO %%a, %%b, %%c
+        )
+    )
+)
