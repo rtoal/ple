65dc3df82bf01cde63ef0b6c70d3f1a40b8e0ad1
Wesley Ng <wesleyng2003@gmail.com>
2025-10-23 12:24:18 -0700

    Improved Batch

--- batch/clockhands.bat
+++ batch/clockhands.bat
@@ -3,18 +3,13 @@ FOR /L %%i IN (0, 1, 10) DO CALL :FOO %%i
 GOTO :END
 
 :FOO
-SETLOCAL ENABLEDELAYEDEXPANSION
 SET /A t=(43200 * %1 + 21600) / 11
 SET /A h=%t% / 3600
 SET /A m=%t% / 60 %% 60
 SET /A s=%t% %% 60
 IF %h% EQU 0 SET h=12
-SET h=0%h%
-SET m=0%m%
-SET s=0%s%
-SET h=!h:~-2!
-SET m=!m:~-2!
-SET s=!s:~-2!
+IF %h% LSS 10 SET h=0%h%
+IF %m% LSS 10 SET m=0%m%
+IF %s% LSS 10 SET s=0%s%
 ECHO %h%:%m%:%s%
-ENDLOCAL
 :END
