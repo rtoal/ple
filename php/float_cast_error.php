cbe9c1a309d029819495e971d548d73ddd925a72
Wesley Ng <wesleyng2003@gmail.com>
2025-10-27 11:52:05 -0700

    Added float cast error

---
+++ php/float_cast_error.php
@@ -0,0 +1,13 @@
+<?php
+    $n = (0.1 + 0.7) * 10;
+
+    echo $n;
+    echo "\n";
+    echo (int)($n);
+    echo "\n";
+    if ($n == 8){
+        echo "8 == 8";
+    } else {
+        echo "8 != 8";
+    }
+?>
--- php/test.ps1
+++ php/test.ps1
@@ -12,8 +12,10 @@ $Error.clear()
 php "$PSScriptRoot\clockhands.php" | 
     Compare-Object (Get-Content "$PSScriptRoot\..\test\clockhands_expected") |
     Assert-MatchTests &&
+php "$PSScriptRoot\float_cast_error.php" &&
 php "$PSScriptRoot\hello_world.php" &&
 php "$PSScriptRoot\html_escaping.php" &&
+php "$PSScriptRoot\integer_overflow.php" &&
 php "$PSScriptRoot\ternary_madness.php" &&
 php "$PSScriptRoot\triples.php" | 
     Compare-Object (Get-Content "$PSScriptRoot\..\test\triple_expected") | 
