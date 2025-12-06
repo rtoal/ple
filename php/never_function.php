6179cab292de337a3dd80f13fb7e6368c8c79159
Wesley Ng <wesleyng2003@gmail.com>
2025-12-06 15:01:51 -0800

    Added beluga logos

---
+++ php/never_function.php
@@ -0,0 +1,10 @@
+<?php
+    function neverFunction(): never
+    {
+        echo "Inside the function";
+        exit();     // Throws error if this line is commented.
+    }
+
+    neverFunction();
+    echo "Outside the function";
+?>
---
+++ php/void_function.php
@@ -0,0 +1,8 @@
+<?php
+    function voidReturn(): void
+    {
+
+    }
+    
+    echo gettype(voidReturn());
+?>
