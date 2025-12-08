258c9589b819bbf304c7a7d77262599e155190ff
Wesley Ng <wesleyng2003@gmail.com>
2025-12-08 14:40:17 -0800

    Cleaned up redundant code

--- gdscript/anagrams.gd
+++ gdscript/anagrams.gd
@@ -16,8 +16,6 @@ func _init():
 	var args = Array(OS.get_cmdline_args())
 	if len(args) != 3:
 		print("ERROR: There must be exactly 1 argument.")
-		quit()
-		return
 	else:
 		generate_permutations(args[2].split(), len(args[2]) - 1)
 	quit()
