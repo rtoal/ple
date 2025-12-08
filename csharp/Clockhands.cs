a2f38a82fc6a019d945c410c4615500ccbb074c4
Wesley Ng <wesleyng2003@gmail.com>
2025-12-08 14:27:03 -0800

    Simplified

--- csharp/Clockhands.cs
+++ csharp/Clockhands.cs
@@ -8,7 +8,7 @@ static class AntiparallelClockHandsComputer
         {
             var t = (43200 * i + 21600) / 11;
             (var h, var m, var s) = (t / 3600, t / 60 % 60, t % 60);
-            Console.WriteLine("{0:00}:{1:00}:{2:00}", (h != 0 ? h : 12), m, s);
+            Console.WriteLine("{0:00}:{1:00}:{2:00}", h != 0 ? h : 12, m, s);
         }
     }
 }
