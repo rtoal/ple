e66e97b974bc73f7a9e885aac0a6379bbf8b0328
Wesley Ng <wesleyng2003@gmail.com>
2025-12-08 16:02:29 -0800

    Added stuff

--- php/test.ps1
+++ php/test.ps1
@@ -16,11 +16,13 @@ php "$PSScriptRoot\float_cast_error.php" &&
 php "$PSScriptRoot\hello_world.php" &&
 php "$PSScriptRoot\html_escaping.php" &&
 php "$PSScriptRoot\integer_overflow.php" &&
+php "$PSScriptRoot\never_function.php" &&
 php "$PSScriptRoot\ternary_madness.php" &&
 php "$PSScriptRoot\triples.php" | 
     Compare-Object (Get-Content "$PSScriptRoot\..\test\triple_expected") | 
     Assert-MatchTests &&
-php "$PSScriptRoot\truthy.php"
+php "$PSScriptRoot\truthy.php" &&
+php "$PSScriptRoot\void_function.php" &&
 ForEach-Object 'foo'
 
 if ($Error -or !$?) {
