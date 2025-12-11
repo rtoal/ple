df3f0ae0a4ef1e3e4cca867090220db1096c3c4a
Wesley Ng <wesleyng2003@gmail.com>
2025-12-11 00:29:58 -0800

    reformat

--- php/float_cast_error.php
+++ php/float_cast_error.php
@@ -6,10 +6,10 @@ echo $n;
 echo "\n";
 echo (int)($n);
 echo "\n";
-if ($n == 8){
-    echo "8 == 8";
+if ($n === 8){
+    echo "8 === 8";
 } else {
-    echo "8 != 8";
+    echo "8 !== 8";
 }
 
 ?>
