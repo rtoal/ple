eaef190bd9358bdca9ac90ea9e84f3d48d801898
Wesley Ng <wesleyng2003@gmail.com>
2026-06-05 04:57:12 -0700

    Changed ses function to have filter, map, and reduce functions.

--- gdscript/sum_of_even_squares.gd
+++ gdscript/sum_of_even_squares.gd
@@ -1,10 +1,7 @@
 extends SceneTree
 
 func sum_of_even_squares(a):
-	var sum = 0
-	for x in a.filter(func (x): return x % 2 == 0):
-		sum += x ** 2
-	return sum
+	return a.filter(func (n): return n % 2 == 0).map(func (n): return n ** 2).reduce(func (acc, n): return acc + n, 0)
 
 func _init():
 	assert(sum_of_even_squares([]) == 0)
